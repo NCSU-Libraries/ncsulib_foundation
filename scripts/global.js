c84c571b1de95c02bb138d8b0e895b9995ade5a5
cdmo <cdmorris22@gmail.com>
2014-01-16 14:39:18 -0500

    Erik Olson replaced megamenu javascript with custom megas styled by _header.scss and class manipulation in primary-nav.js

--- scripts/global.js
+++ scripts/global.js
@@ -1,6 +1,4 @@
 jQuery(document).ready(function ($) {
-  $("#mega").dcMegaMenu();
-
   // polyfill for IE<10, this is used anywhere we have an input element with
   // placeholder text, for example, the search in the header
   $("input, textarea").placeholder();
