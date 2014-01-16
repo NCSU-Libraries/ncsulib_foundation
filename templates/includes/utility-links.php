414bac9d44bdb2b2aee08227ca3c07686aaa7495
cdmo <cdmorris22@gmail.com>
2014-01-16 14:51:25 -0500

    added globalchat class

--- templates/includes/utility-links.php
+++ templates/includes/utility-links.php
@@ -5,6 +5,6 @@
 		<li><a href="/hours/">HOURS</a></li>
 		<li><a href="/faq/">FAQ</a></li>
 		<li><a href="https://www.lib.ncsu.edu/website/logout.php">LOG OUT</a></li>
-		<li><a href="http://libraryh3lp.com/chat/ref-desk@chat.libraryh3lp.com">CHAT NOW</a></li>
+		<li><a href="http://libraryh3lp.com/chat/ref-desk@chat.libraryh3lp.com" class="globalchat">CHAT NOW</a></li>
 	</ul>
 </nav>
