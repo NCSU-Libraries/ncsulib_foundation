4b904666f7927c945356cd6c67a12ab55ac40ef7
cdmo <cdmorris22@gmail.com>
2014-01-16 14:03:42 -0500

    Erik Olson moved hover intent to primary-nav.js

--- scripts/global.js
+++ scripts/global.js
@@ -9,7 +9,4 @@ jQuery(document).ready(function ($) {
   // polyfill for IE<10, this is used anywhere we have an input element with
   // placeholder text, for example, the search in the header
   $("input, textarea").placeholder();
-
-	// set hover intent for global nav
-	$('#primary-nav li a').hoverIntent(settings);
 });
