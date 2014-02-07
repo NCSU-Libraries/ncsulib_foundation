b9ed69a0660d495a39d272761e1b4849dd953ba9
Erik Olson <eolson@sandbox.(none)>
2014-02-07 15:01:22 -0500

    removed

--- scripts/polyfill/modernizr-tests.js
+++
@@ -1,6 +0,0 @@
-// Input placeholder test (IE 8,9 polyfill)
- Modernizr.load({
-   test: Modernizr.input.placeholder,
-     nope: ['/sites/all/themes/ncsulib_foundation/scripts/minified/jquery.placeholder.min.js',
-             '/sites/all/themes/ncsulib_foundation/scripts/polyfill/placeholder.js']
-});
