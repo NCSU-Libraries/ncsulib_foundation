d451234385e388fd9e04674b98f5283602045a4c
Charlie Morris <charlie_morris@ncsu.edu>
2014-02-24 16:29:40 -0500

    grunt to create aggregated javascript file to be used in htdocs

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,9 @@
 module.exports = function(grunt) {
 
-//
-// look into hashing
-//
-
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    clean: ['templates/includes/scripts/*'],
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
@@ -14,12 +11,22 @@ module.exports = function(grunt) {
         preserveComments: false
       },
       build: {
-        src: 'scripts/*.js',
-        dest: '<%= pkg.name %>.min.js'
+        src: ['scripts/modernizr-tests.js',
+        'scripts/primary-nav.js',
+        'scripts/search-bar.js',
+        'scripts/vendor/jquery.autocomplete.js',
+        'scripts/vendor/jquery.mockjax.js',
+        'scripts/vendor/jquery.hoverIntent.minified.js',
+        'scripts/global.js'],
+        dest: 'templates/includes/scripts/<%= pkg.name %>.min.js'
       }
-    },
+    }
   });
 
-  // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-hash');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  // clean out the old js, compress new js
+  grunt.registerTask('default', ['clean', 'uglify']);
 };
