140d9e95ca855e8c48db69d033f56fda5f193217
Charlie Morris <charlie_morris@ncsu.edu>
2014-02-26 09:10:39 -0500

    search bar 1.0 re-added for Foundation 5 update

--- scripts/search-bar.js
+++ scripts/search-bar.js
@@ -4,12 +4,27 @@ var searchbar = {
 		jQuery('#search-submit').click(function(e){
 			jQuery(this).addClass('active');
 			jQuery('#search-all').addClass('active');
+		});
 
-			// e.preventDefault();
-		})
-	}
-}
+    // show or hide for small breakpoint
+    jQuery('#search-toggle').click( function() {
+      jQuery('#utility-search').toggle('slow');
+    });
+
+    // show the searchbar if the window is resized over the medium breakpoint
+    jQuery(window).resize( function() {
+      searchbar.checkwidth();
+    });
+	},
+  checkwidth : function() {
+    var windowsize = jQuery(window).width();
+    if (windowsize > searchbar.mediumBreakpoint) {
+      jQuery('#utility-search').show();
+    }
+  },
+  mediumBreakpoint: 767
+};
 
 jQuery(function(){
 	searchbar.init();
-})
+});
