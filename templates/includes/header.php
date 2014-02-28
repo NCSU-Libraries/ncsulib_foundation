2c31112337079e7fa365138a06178ce130481605
Erik Olson <eolson@ncsu.edu>
2014-02-28 13:19:10 -0500

    added absolute links so includes can be used in external apps

--- templates/includes/header.php
+++ templates/includes/header.php
@@ -21,8 +21,8 @@
 	<nav class="top-bar">
 		<ul class="title-area">
 			<li class="name">
-			  	<a href="/" title="<?php print t('NCSU Library'); ?>" rel="home" id="logo">
-					<img src="/sites/all/themes/ncsulib_foundation/images/ncsu-library-logo-white.png" alt="<?php print t('Home'); ?>" />
+			  	<a href="//<?php echo $_SERVER['SERVER_NAME']; ?>" title="<?php print t('NCSU Library'); ?>" rel="home" id="logo">
+					<img src="//<?php echo $_SERVER['SERVER_NAME']; ?>/sites/all/themes/ncsulib_foundation/images/ncsu-library-logo-white.png" alt="<?php print t('Home'); ?>" />
 			  	</a>
 			</li>
 			<li id="nav-toggle" class="right-off-canvas-toggle">
@@ -44,74 +44,74 @@
 <nav id="primary-nav-menus">
 	<div id="menu-find" class="primary-menu-list" >
 		<ul>
-			<li><a href="/catalog">Books &amp; Media</a></li>
-			<li><a href="/articles">Articles</a></li>
-			<li><a href="/journals">Journal Titles</a></li>
-			<li><a href="/databases">Databases</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/catalog">Books &amp; Media</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/articles">Articles</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/journals">Journal Titles</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/databases">Databases</a></li>
 			<li><a href="https://reserves.lib.ncsu.edu">Course Reserves</a></li>
 		</ul>
 		<ul>
-			<li><a href="/data">Spatial &amp; Numerical Data</a></li>
-			<li><a href="/specialcollections">Special Collections</a></li>
-			<li><a href="/repository">NC State Publications</a></li>
-			<li><a href="/repository">Theses &amp; Dissertations</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/data">Spatial &amp; Numerical Data</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/specialcollections">Special Collections</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/repository">NC State Publications</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/repository">Theses &amp; Dissertations</a></li>
 		</ul>
 	</div>
 	<div id="menu-help" class="primary-menu-list">
 		<ul>
-			<li><a href="/askus">Ask Us</a></li>
-			<li><a href="/researchconsultation">Research Consultation</a></li>
-			<li><a href="/digital-media-lab/techconsult">Technology Consultation</a></li>
-			<li><a href="/researchworkshops">Research Workshops</a></li>
-			<li><a href="/cdsc">Copyright Guidance</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/askus">Ask Us</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/researchconsultation">Research Consultation</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/digital-media-lab/techconsult">Technology Consultation</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/researchworkshops">Research Workshops</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/cdsc">Copyright Guidance</a></li>
 		</ul>
 		<ul>
-			<li><a href="/course">Course Tools</a></li>
-			<li><a href="/tools-citation">Citation</a></li>
-			<li><a href="/reference-tools">Reference</a></li>
-			<li><a href="/guides">Guides</a></li>
-			<li><a href="/tutorials">Tutorials</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/course">Course Tools</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/tools-citation">Citation</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/reference-tools">Reference</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/guides">Guides</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/tutorials">Tutorials</a></li>
 		</ul>
 	</div>
 	<div id="menu-services" class="primary-menu-list">
 		<ul>
-			<li><a href="/borrow">Borrow, Renew, Request</a></li>
-			<li><a href="/computing">Computing</a></li>
-			<li><a href="/digital-media-lab">Create Digital Media</a></li>
-			<li><a href="/disabilityservices">Disability Services</a></li>
-			<li><a href="/distance">Distance Learning</a></li>
-			<li><a href="/faculty">Faculty/Instructor Support</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/borrow">Borrow, Renew, Request</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/computing">Computing</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/digital-media-lab">Create Digital Media</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/disabilityservices">Disability Services</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/distance">Distance Learning</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/faculty">Faculty/Instructor Support</a></li>
 		</ul>
 		<ul>
-			<li><a href="/groupfinder">GroupFinder</a></li>
-			<li><a href="/reservearoom">Reserve a Room</a></li>
-			<li><a href="/printing">Print, Copy, Scan</a></li>
-			<li><a href="/collections/suggestpurchase">Suggest a Purchase</a></li>
-			<li><a href="/techlending">Technology Lending</a></li>
-			<li><a href="/tripsaver">Tripsaver</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/groupfinder">GroupFinder</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/reservearoom">Reserve a Room</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/printing">Print, Copy, Scan</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/collections/suggestpurchase">Suggest a Purchase</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/techlending">Technology Lending</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/tripsaver">Tripsaver</a></li>
 		</ul>
 	</div>
 	<div id="menu-libraries" class="primary-menu-list">
 		<ul>
-			<li><a href="/">D. H. Hill Library</a></li>
-			<li><a href="/huntlibrary">James B. Hunt Jr. Library</a></li>
-			<li><a href="/design">Design Library</a></li>
-			<li><a href="/nrl">Natural Resources Library</a></li>
-			<li><a href="/vetmed">Veterinary Medicine Library</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/">D. H. Hill Library</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/huntlibrary">James B. Hunt Jr. Library</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/design">Design Library</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/nrl">Natural Resources Library</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/vetmed">Veterinary Medicine Library</a></li>
 		</ul>
 	</div>
 	<div id="menu-about" class="primary-menu-list">
 		<ul>
-			<li><a href="/contact">Contact Us</a></li>
-			<li><a href="/parking">Directions &amp; Parking</a></li>
-			<li><a href="/hours">Hours</a></li>
-			<li><a href="/jobs">Jobs</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/contact">Contact Us</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/parking">Directions &amp; Parking</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/hours">Hours</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/jobs">Jobs</a></li>
 		</ul>
 		<ul>
-			<li><a href="/staff">Staff</a></li>
-			<li><a href="/directories">Directories</a></li>
-			<li><a href="/giving/friends">Friends of the Library</a></li>
-			<li><a href="/specialcollections">Special Collections</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/staff">Staff</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/directories">Directories</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/giving/friends">Friends of the Library</a></li>
+			<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/specialcollections">Special Collections</a></li>
 		</ul>
 	</div>
 </nav>
@@ -122,8 +122,8 @@
 		<div id="home-hours">
 			<p id="hours-title"><a href="/hours">TODAY'S HOURS</a>:</p>
 			<ul>
-				<li><a href="/hours/hill/general"><span class="library">D. H. Hill:</span> &nbsp; <span class="hours">Open 24 Hours</span></a></li>
-				<li><a href="/hours/hunt/general"><span class="library">James B. Hunt Jr.:</span> &nbsp; <span class="hours">Open 24 Hours</span></a></li>
+				<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/hours/hill/general"><span class="library">D. H. Hill:</span> &nbsp; <span class="hours">Open 24 Hours</span></a></li>
+				<li><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>/hours/hunt/general"><span class="library">James B. Hunt Jr.:</span> &nbsp; <span class="hours">Open 24 Hours</span></a></li>
 			</ul>
 		</div>
 		<?php else: ?>
