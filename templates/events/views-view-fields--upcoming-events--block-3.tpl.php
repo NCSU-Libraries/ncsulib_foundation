52b72e0bc1794da79789a4bc7e7afe7ef677f168
Erik Olson <eolson@ncsu.edu>
2014-03-19 15:32:48 -0400

    responsifying layout

--- templates/events/views-view-fields--upcoming-events--block-3.tpl.php
+++ templates/events/views-view-fields--upcoming-events--block-3.tpl.php
@@ -16,16 +16,16 @@
 	<div class="columns large-8">
 		<?php foreach ($fields as $id => $field): ?>
 			<?php if($field->class == 'title'): ?>
-			<h2><?php echo $field->content; ?></h2>
+			<h2 class="subheader"><?php echo $field->content; ?></h2>
 			<?php endif; ?>
 			<?php if($field->class == 'field-time-1'): ?>
-			<h4><?php echo strip_tags($field->content, '<a>'); ?></h4>
+			<h4 class="subheader"><?php echo strip_tags($field->content, '<a>'); ?></h4>
 			<?php endif; ?>
 			<?php if($field->class == 'field-time'): ?>
-			<h6><?php echo $field->content; ?></h6>
+			<h6 class="subheader"><?php echo $field->content; ?></h6>
 			<?php endif; ?>
 			<?php if($field->class == 'field-space'): ?>
-			<h6><?php echo $field->content; ?></h6>
+			<h6 class="subheader"><?php echo $field->content; ?></h6>
 			<?php endif; ?>
 		<?php endforeach; ?>
 	</div>
