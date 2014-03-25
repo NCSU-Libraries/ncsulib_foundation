c0bf542959fdd4292e87cb8221184e12d2279833
Erik Olson <eolson@ncsu.edu>
2014-03-25 17:07:24 -0400

    responsification

--- templates/node--departments.tpl.php
+++ templates/node--departments.tpl.php
@@ -1,12 +1,13 @@
+<h1><?= $title; ?></h1>
 <div class="row">
-	<div class="large-8 columns">
+	<div class="medium-8 columns">
 		<p><?php print strip_tags(render($content['field_dept_about'])); ?></p>
 
 		<h2>Staff</h2>
-		<?php print render($content['field_dept_head']); ?>
-		<?php print render($content['field_dept_assochead']); ?>
-		<?php print render($content['field_dept_assisthead']); ?>
-		<?php print render($content['field_dept_staff']); ?>
+		<?= render($content['field_dept_head']); ?>
+		<?= render($content['field_dept_assochead']); ?>
+		<?= render($content['field_dept_assisthead']); ?>
+		<?= render($content['field_dept_staff']); ?>
 
 		<?php if ($content['field_dept_location']): ?>
 			<h2>Location</h2>
@@ -24,15 +25,13 @@
 		<br />
 		<p><a href="/departments">&larr; Back to All Departments &amp; Units</a></p>
 	</div>
-	<div class="large-3 columns">
-		<div class="panel">
-			<h2>Contact</h2>
-			<?php if ($content['field_dept_phone']): ?>
-				<?php print render($content['field_dept_phone']); ?>
-			<?php endif ?>
-			<?php if ($content['field_dept_email']): ?>
-				<?php print render($content['field_dept_email']); ?>
-			<?php endif ?>
-		</div>
+	<div class="medium-3 columns">
+		<h2>Contact</h2>
+		<?php if ($content['field_dept_phone']): ?>
+			<?php print render($content['field_dept_phone']); ?>
+		<?php endif ?>
+		<?php if ($content['field_dept_email']): ?>
+			<?php print render($content['field_dept_email']); ?>
+		<?php endif ?>
 	</div>
 </div>
