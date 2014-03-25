7124400a9740e75c0cd7a1260a0e3cc568462eaf
Erik Olson <eolson@ncsu.edu>
2014-03-25 16:34:35 -0400

    responsification

--- templates/scrc/block--block--99.tpl.php
+++ templates/scrc/block--block--99.tpl.php
@@ -1,11 +1,11 @@
 <div id="scrc-news">
 	<h2>Latest News</h2>
-	<ul id="news-feed">
+	<dl id="news-feed">
 	<?php
 		$xml = file_get_contents('http://feeds.feedburner.com/NCSULibrariesHistoricallyStated?format=xml');
 		$feed = simplexml_load_string($xml);
 
-		for($i=0;$i<2;$i++):
+		for($i=0;$i<3;$i++):
 			$title = $feed->channel->item[$i]->title;
 			$description = substr($feed->channel->item[$i]->description, 0, 200).'...';
 			$link = $feed->channel->item[$i]->link;
@@ -15,20 +15,20 @@
 			preg_match('/<img.*?src="(.*?)"/si',$content->encoded,$img);
 			$img_url = preg_split('/http:\/\//si', $img[1]);
 	?>
-		<li>
-			<div class="img">
+		<dd class="row">
+			<div class="medium-4 columns">
 				<img src="<?php echo 'http://'.$img_url[1]; ?>" width="100%" />
 			</div>
-			<div class="content">
-				<h3><a href="<?php echo $link; ?>"><?php echo $title ?></a></h3>
+			<div class="medium-8 columns">
+				<h3 class="subheader"><a href="<?php echo $link; ?>"><?php echo $title ?></a></h3>
 				<p class="date"><?php echo $date; ?></p>
 				<p><?php echo $description; ?></p>
 				<a href="<?php echo $link; ?>">Read More</a>
 			</div>
-		</li>
+		</dd>
 
 	<?php endfor; ?>
-	</ul>
+	</dl>
 	<div class="view-all">
 		<a href="http://news.lib.ncsu.edu/scrc/">View all SCRC news >></a>
 	</div>
