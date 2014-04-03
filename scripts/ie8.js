38318030f9145ca72520cdf213ea96deb0e70bea
Erik Olson <eolson@ncsu.edu>
2014-04-03 15:35:35 -0400

    ie8 message for users who use ie8 to upgrade browsers

---
+++ scripts/ie8.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function(){
+
+	var str = "<div id='browser_warning'><h4>The NCSU Library's website is not optimized for your browser.<br/> Would you consider using a <a href='http://browsehappy.com/'>modern browser</a>?</h4></div>";
+
+	jQuery('body').prepend(str);
+});
--- templates/includes/head.php
+++ templates/includes/head.php
@@ -49,10 +49,12 @@
   })(jQuery, Drupal, this, this.document);
 </script>
 <?php endif; ?>
+
 <!--[if lt IE 9]>
 <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 <link rel="stylesheet" type="text/css" href="//webdev.lib.ncsu.edu/sites/all/themes/ncsulib_foundation/styles/core/layout/ie8.css">
 <script type="text/javascript" src="//webdev.lib.ncsu.edu/sites/all/themes/ncsulib_foundation/scripts/vendor/rem.js"></script>
+<script type="text/javascript" src="//webdev.lib.ncsu.edu/sites/all/themes/ncsulib_foundation/scripts/ie8.js"></script>
 <![endif]-->
 
 <!--[if IE 9]>
