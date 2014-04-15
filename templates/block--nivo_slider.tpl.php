95ab4419a3747d1e873a67702a1fda6995cf18b8
Erik Olson <eolson@ncsu.edu>
2014-04-15 17:15:04 -0400

    adding ga tracking to artbox

--- templates/block--nivo_slider.tpl.php
+++ templates/block--nivo_slider.tpl.php
@@ -1,15 +1,24 @@
 <?php
 	$slider_vars = variable_get('nivo_slider_banner_settings', array());
 ?>
-<ul class="orbit-slider" data-orbit="">
+<ul id="artbox" class="orbit-slider" data-orbit="">
 
 <?php
 	foreach($slider_vars as $slide):
 		if($slide['published']):
 		$file = file_load($slide['fid']);
 		$url = $slide['url'];
+		preg_match('/\/\/(.*?)\//si', $url, $url_domain);
+		$url_pos = strpos($url_domain[1], 'lib.ncsu.edu');
+		if($url_pos){
+			$url_dest = 'internal';
+		} else{
+			$url_dest = 'external';
+		}
+		$title = $slide['title'];
+
 ?>
-    <li><a href="<?= $url; ?>"><img src="<?= file_create_url($file->uri); ?>" /></a></li>
+    <li><a href="<?= $url; ?>" data-title="<?= $title; ?>" data-destination="<?= $url_dest; ?>"><img src="<?= file_create_url($file->uri); ?>" /></a></li>
 	<?php endif; ?>
 <?php endforeach; ?>
 
