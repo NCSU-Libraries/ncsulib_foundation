26e7d341c085db00677c501ab31f9eb99aa0ac05
Charlie Morris <charlie_morris@ncsu.edu>
2014-04-16 12:18:51 -0400

    adding modernizr test for touch

--- scripts/modernizr-tests.js
+++ scripts/modernizr-tests.js
@@ -1,6 +1,13 @@
-// Input placeholder test (IE 8,9 polyfill)
-Modernizr.load({
-    test: Modernizr.input.placeholder,
-    nope: ['/sites/all/themes/ncsulib_foundation/scripts/vendor/jquery.placeholder.js']
-  }
-);
+Modernizr.load([
+    // Input placeholder test (IE 8,9 polyfill)
+    {
+        test: Modernizr.input.placeholder,
+        nope: ['/sites/all/themes/ncsulib_foundation/scripts/vendor/jquery.placeholder.js']
+    },
+
+    // Touch events
+    {
+        test: Modernizr.touch,
+        yep: '/sites/all/themes/ncsulib_foundation/scripts/touch.js',
+    }
+]);
