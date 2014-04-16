3517ffdcf3e1532e5b88008fd9c1d5dcbb87696e
Erik Olson <eolson@ncsu.edu>
2014-04-16 12:52:00 -0400

    new js file just for artbox functonality

---
+++ scripts/home-artbox.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function($){
+
+  $(document).foundation({
+    orbit: {
+      animation: 'fade',
+      slide_number: false,
+      stack_on_small: true
+    }
+  });
+
+});
