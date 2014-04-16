c3d0b7123ec3111d69d348c02506f6c5f974aca2
Charlie Morris <charlie_morris@ncsu.edu>
2014-04-16 13:04:06 -0400

    made load call absolute

--- scripts/modernizr-tests.js
+++ scripts/modernizr-tests.js
@@ -2,12 +2,12 @@ Modernizr.load([
     // Input placeholder test (IE 8,9 polyfill)
     {
         test: Modernizr.input.placeholder,
-        nope: ['/sites/all/themes/ncsulib_foundation/scripts/vendor/jquery.placeholder.js']
+        nope: '//webdev.lib.ncsu.edu/sites/all/themes/ncsulib_foundation/scripts/vendor/jquery.placeholder.js'
     },
 
     // Touch events
     {
         test: Modernizr.touch,
-        yep: '/sites/all/themes/ncsulib_foundation/scripts/touch.js',
+        yep: '//webdev.lib.ncsu.edu/sites/all/themes/ncsulib_foundation/scripts/touch.js',
     }
 ]);
