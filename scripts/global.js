903b2b897f7ef0f1c1d5432a62e769b12892d719
Erik Olson <eolson@ncsu.edu>
2014-05-13 12:13:53 -0400

    add more parameters to orbit slider

--- scripts/global.js
+++ scripts/global.js
@@ -15,7 +15,7 @@ function slider_check(){
 
 	if(jQuery('.orbit-slider').length > 0 && jQuery('.orbit-slider').attr('data-orbit') != ''){
 
-		jQuery('.orbit-slider').attr('data-orbit', '').attr('data-options', 'animation:fade;slide_number:false;');
+		jQuery('.orbit-slider').attr('data-orbit', '').attr('data-options', 'animation:fade;slide_number:false;animation_speed:300;timer_speed:5000;');
 
 	}
 }
