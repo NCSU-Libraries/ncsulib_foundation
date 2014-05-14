6c563eaf6fe1bf795fa1753ef3fc973e782c4aa0
Erik Olson <eolson@ncsu.edu>
2014-05-14 09:13:01 -0400

    added summer hours pdf to hours page

--- templates/hours/block--block--93.tpl.php
+++ templates/hours/block--block--93.tpl.php
@@ -59,5 +59,6 @@
 <h2>Download Hours Schedule</h2>
 <ul id="hours-download">
 	<li><img src="/images/pdf.gif" alt="PDF icon"><a href="/documents/hours/2014-spring-hours.pdf">Spring 2014 hours</a></li>
+	<li><img src="/images/pdf.gif" alt="PDF icon"><a href="/documents/hours/2014-summer-hours.pdf">Summer 2014 hours</a></li>
 </ul>
 
