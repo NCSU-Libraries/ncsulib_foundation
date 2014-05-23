df44ae4c8f840b81918c019d3a410b586e709c06
Erik Olson <eolson@ncsu.edu>
2014-05-23 12:19:29 -0400

    fixed issue where multiple search inputs were causing conflicts

--- scripts/search-cancel-button.js
+++ scripts/search-cancel-button.js
@@ -1,95 +1,124 @@
 var clear = {
 	button : '<a href="#" class="search-cancel-button"><i class="fa fa-times-circle"></i></a>',
-	buttonIsAdded : false,
 
 	init: function(){
 		// add button to page on focus
 		jQuery("input[type='search']").on('input', function(e){
 
-			var val = jQuery(this).val();
-			if(!clear.input && val.length != 0){
-				clear.input = jQuery(this);
-				clear.activate_button();
-			 } else if(val.length == 0 && clear.buttonIsAdded){ //remove button if text has been deleted
-				clear.deactivate_button();
-			}
+			clear.input = jQuery(this);
+			clear.input.attr('data-active', 'true');
 
+			if(clear.input.attr('data-has_button') !== 'true'){
+				clear.activate_button();
+			}
 		})
+
+
+		jQuery("input[type='search']").focus(function(e){
+			clear.input = jQuery(this);
+			clear.input.attr('data-active', 'true');
+		})
+
 
 		// remove button from page on blur
 		jQuery("input[type='search']").blur(function(e){
-			var val = jQuery(this).val();
-			if(val.length == ''){
-				clear.deactivate_button();
+
+			if(clear.input){
+				clear.input.attr('data-active', 'false');
+
+				if(clear.input.val().length === ''){
+					clear.deactivate_button();
+				}
 			}
+
 		})
 
-		jQuery("input[type='search']").focus(function(e){
-			var val = jQuery(this).val();
-
-			if(val.length != 0 && !clear.buttonIsAdded){ //if input has any text
-				clear.activate_button();
-			}
-		})
 
 		// resize in case some jerk decises to resize the browser while the cancel button is visible
 		window.onresize = function(){
-			if(clear.buttonIsAdded){
-				clear.position_button();
-			}
+			clear.position_button();
 		}
 	},
 
 	activate_button : function(){
-			clear.input.after(clear.button);
-			clear.buttonIsAdded = true;
+		clear.input.after(clear.button); //add cancel button
+		clear.input.attr('data-has_button', 'true');
 
-			// set position of button
-			clear.position_button();
-			clear.enable_button();
+		// set position of button
+		clear.position_button();
+		clear.enable_button();
 	},
 
 	deactivate_button : function(){
-		jQuery('.search-cancel-button').remove();
-		jQuery(clear.input).css('padding-right', clear.inputPaddingLeft);
+		clear.input.next().remove();
+		clear.input.attr('data-has_button', 'false');
 
+		clear.input.css('padding-right', clear.inputPaddingLeft);
 		clear.input = undefined;
-		clear.buttonIsAdded = false;
 	},
 
 	position_button : function(){
-		clear.inputPaddingLeft = jQuery(clear.input).css('padding-left');
-		console.log(clear.inputPaddingLeft);
-		var button = jQuery('.search-cancel-button');
-		var input_font = jQuery(clear.input).css('font-size');
-		var input_font_size = input_font.split('px');
-		var input_font_size = Number(input_font_size[0]);
-		var input_height = jQuery(clear.input).outerHeight();
-		var button_width = input_font_size+15;
+		// clear.inputPaddingLeft = jQuery(clear.input).css('padding-left');
+		// var button = jQuery('.search-cancel-button');
+		// var input_font = jQuery(clear.input).css('font-size');
+		// var input_font_size = input_font.split('px');
+		// var input_font_size = Number(input_font_size[0]);
+		// var input_height = jQuery(clear.input).outerHeight();
+		// var button_width = input_font_size+15;
 
-		jQuery(button).css({
-			'float' : 'left',
-			'margin-left' : 0
-		});
+		// jQuery(button).css({
+		// 	'float' : 'left',
+		// 	'margin-left' : 0
+		// });
 
-		jQuery('.search-cancel-button i').css({
-			'font-size' : input_font_size+'px',
-			'left' : -button_width+'px',
-			'height' : input_height,
-			'padding-top' : (input_height-input_font_size)*0.5
-		});
+		// jQuery('.search-cancel-button i').css({
+		// 	'font-size' : input_font_size+'px',
+		// 	'left' : -button_width+'px',
+		// 	'height' : input_height,
+		// 	'padding-top' : (input_height-input_font_size)*0.5
+		// });
 
-		// add padding to input box so text doesn't disappear begin cancel button
-		jQuery(clear.input).css({
-			'padding-right' : button_width+5+'px',
-			'float' : 'left'
-		});
+		// // add padding to input box so text doesn't disappear begin cancel button
+		// clear.input.css({
+		// 	'padding-right' : button_width+5+'px',
+		// 	'float' : 'left'
+		// });
+		//
+		jQuery("input[type='search']").each(function(){
+			clear.inputPaddingLeft = jQuery(this).css('padding-left');
+			var button = jQuery('.search-cancel-button');
+			var input_font = jQuery(this).css('font-size');
+			var input_font_size = input_font.split('px');
+			var input_font_size = Number(input_font_size[0]);
+			var input_height = jQuery(this).outerHeight();
+			var button_width = input_font_size+15;
+
+			jQuery(this).parent().find('.search-cancel-button').css({
+				'float' : 'left',
+				'margin-left' : 0
+			});
+
+			jQuery(this).parent().find('.search-cancel-button i').css({
+				'font-size' : input_font_size+'px',
+				'left' : -button_width+'px',
+				'height' : input_height,
+				'padding-top' : (input_height-input_font_size)*0.5
+			});
+
+			// add padding to input box so text doesn't disappear begin cancel button
+			jQuery(this).css({
+				'padding-right' : button_width+5+'px',
+				'float' : 'left'
+			});
+
+		})
 
 	},
 
 	enable_button : function(){
 		jQuery(".search-cancel-button").click(function(e){
 
+			clear.input = jQuery(this).parent().find("input[type='search']");
 			clear.input.val('');
 			clear.input.focus();
 			clear.deactivate_button();
