3dfa1118cef42eda134434123c1d548efbfdf823
Erik Olson <eolson@ncsu.edu>
2014-05-29 10:16:47 -0400

    added js for books & media tab

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -2,9 +2,10 @@
 drupal_add_css(drupal_get_path('theme', 'ncsulib_foundation').'/styles/core/custom/home.css', array('weight' => 998, 'group' => 101));
 drupal_add_css(drupal_get_path('theme', 'ncsulib_foundation').'/styles/typeahead.css', array('weight' => 999, 'group' => 101));
 drupal_add_js(drupal_get_path('theme', 'ncsulib_foundation').'/scripts/home-artbox.js');
+drupal_add_js('/catalog/scripts/app.js');
 drupal_add_js(drupal_get_path('theme', 'ncsulib_foundation').'/scripts/availability-data.js');
 drupal_add_js(drupal_get_path('theme', 'ncsulib_foundation').'/scripts/home-tracking-events.js');
-drupal_add_js(drupal_get_path('theme', 'ncsulib_foundation').'/scripts/vendor/trln_autosuggest.min.js');
+drupal_add_js(drupal_get_path('theme', 'ncsulib_foundation').'/scripts/vendor/trln_autosuggest.js');
 drupal_add_js(drupal_get_path('theme', 'ncsulib_foundation').'/scripts/vendor/trln_autocomplete.js');
 drupal_add_js(drupal_get_path('theme', 'ncsulib_foundation').'/scripts/books-media-tab-autosuggest.js');
 drupal_add_js(drupal_get_path('theme', 'ncsulib_foundation').'/scripts/home-search-tabs-logging.js');
