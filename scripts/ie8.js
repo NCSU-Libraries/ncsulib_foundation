3761f35e093741e25c23a11b7349b6425ae6cc1c
Erik Olson <eolson@ncsu.edu>
2014-06-03 15:58:58 -0400

    fixed styling issues for ie8

--- scripts/ie8.js
+++ scripts/ie8.js
@@ -1,4 +1,3 @@
-	alert('ie8');
 jQuery(document).ready(function($){
 	var str = "<div id='browser_warning'><h4>The NCSU Library's website is not optimized for your browser.<br/> Would you consider using a <a href='http://browsehappy.com/'>modern browser</a>?</h4></div>";
 
