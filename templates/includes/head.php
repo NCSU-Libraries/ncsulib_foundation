e783c69b5f1c5c0deb8c308ae3a9164a618fea92
Erik Olson <eolson@ncsu.edu>
2014-06-03 15:56:27 -0400

    fixed styling issues for ie8

--- templates/includes/head.php
+++ templates/includes/head.php
@@ -62,8 +62,9 @@
 <![endif]-->
 
 <!--[if lt IE 9]>
-<link rel="stylesheet" type="text/css" href="//www.lib.ncsu.edu/sites/all/themes/ncsulib_foundation/styles/core/layout/ie8.css">
+<script src="http://code.jquery.com/jquery-1.7.2.min.js "></script>
+<script type="text/javascript" src="//www.lib.ncsu.edu/sites/all/themes/ncsulib_foundation/scripts/ie8.js"></script>
+<link rel="stylesheet" type="text/css" href="//webdev.lib.ncsu.edu/sites/all/themes/ncsulib_foundation/styles/core/layout/ie8.css">
 <script type="text/javascript" src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 <script type="text/javascript" src="//www.lib.ncsu.edu/sites/all/themes/ncsulib_foundation/scripts/rem.js"></script>
-<script type="text/javascript" src="//www.lib.ncsu.edu/sites/all/themes/ncsulib_foundation/scripts/ie8.js"></script>
 <![endif]-->
