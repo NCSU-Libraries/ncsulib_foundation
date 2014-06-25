bf89749335254262a4204ec85b22305fbcf0f2b7
Erik Olson <eolson@ncsu.edu>
2014-06-25 11:50:25 -0400

    various changes

--- templates/staff/views-view--department--block-1.tpl.php
+++ templates/staff/views-view--department--block-1.tpl.php
@@ -1,4 +1,4 @@
-<select>
+<select id="department-select">
 	<option value="">- Select a Department -</option>
 	<?php foreach($view->result as $result): ?>
 	<option value="<?php echo $result->nid; ?>"><?php echo $result->node_title; ?></option>
--- templates/staff/views-view--staff--block-8.tpl.php
+++ templates/staff/views-view--staff--block-8.tpl.php
@@ -1,5 +1,34 @@
-<?php kpr($view->result); ?>
 <?php
-	$first_name = $view->result->field_field_firstname';
-	print_r($first_name);
+	$alpha_ary = array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
+	$name_ary = array();
+
+	// collect all names
+	foreach($view->result as $person){
+		$first_name = $person->field_field_firstname[0]['rendered']['#markup'];
+		$last_name = $person->field_field_lastname[0]['rendered']['#markup'];
+
+		if($old_letter == $last_name[0]){
+			array_push($name_ary[$last_name[0]], array('first_name'=>$first_name, 'last_name'=>$last_name));
+		} else{
+			$name_ary[$last_name[0]][0] = array('first_name'=>$first_name, 'last_name'=>$last_name);
+		}
+
+		$old_letter = $last_name[0];
+	}
+
  ?>
+
+<!-- layout paginator -->
+<ul class="pagination">
+<?php
+	foreach($alpha_ary as $key=>$letter):
+		if($letter != key($name_ary[strtoupper($letter)])):
+?>
+	<li class="unavailable"><a href=""><?= strtoupper($letter); ?></a></li>
+	<?php else: ?>
+	<li><a href=""><?= strtoupper($letter); ?></a></li>
+	<?php endif; ?>
+<?php endforeach; ?>
+</ul>
+<a href="" class="styled">or view all</a>
+<hr />
