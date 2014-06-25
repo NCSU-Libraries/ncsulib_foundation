fe72a18b8a429e9e5969f9037a1261c281f95131
Charlie Morris <charlie_morris@ncsu.edu>
2014-06-25 15:24:52 -0400

    just to get rid of stupid colons.

--- template.php
+++ template.php
@@ -385,6 +385,33 @@ function ncsulib_foundation_aggregator_block_item($variables) {
 /**
  * Implements theme_field()
  *
+ * Just to get rid of stupid colons
+ */
+function ncsulib_foundation_field($variables) {
+  $output = '';
+
+  // Render the label, if it's not hidden.
+  if (!$variables['label_hidden']) {
+    $output .= '<div class="field-label"' . $variables['title_attributes'] . '>' . $variables['label'] . '&nbsp;</div>';
+  }
+
+  // Render the items.
+  $output .= '<div class="field-items"' . $variables['content_attributes'] . '>';
+  foreach ($variables['items'] as $delta => $item) {
+    $classes = 'field-item ' . ($delta % 2 ? 'odd' : 'even');
+    $output .= '<div class="' . $classes . '"' . $variables['item_attributes'][$delta] . '>' . drupal_render($item) . '</div>';
+  }
+  $output .= '</div>';
+
+  // Render the top-level DIV.
+  $output = '<div class="' . $variables['classes'] . '"' . $variables['attributes'] . '>' . $output . '</div>';
+
+  return $output;
+}
+
+/**
+ * Implements theme_field()
+ *
  * Using this to change the markup delivered to the Field Request Form URL
  * field.  Turning it into a button.
  */
