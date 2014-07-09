65a2121686e83af7b73bf23fcfc8cd74041f7f27
Erik Olson <eolson@ncsu.edu>
2014-07-09 15:34:43 -0400

    slightly changing the aside. main title is h2 instead of h3. h4 etc are no longer all caps

--- templates/block.tpl.php
+++ templates/block.tpl.php
@@ -47,7 +47,7 @@
 
   <?php print render($title_prefix); ?>
   <?php if ($block->subject): ?>
-    <h3<?php print $title_attributes; ?>><?php print $block->subject ?></h3>
+    <h2<?php print $title_attributes; ?>><?php print $block->subject ?></h2>
   <?php endif;?>
   <?php print render($title_suffix); ?>
 
