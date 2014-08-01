7366ce6274970287a5d254ce978b109808ec0b45
Erik Olson <eolson@ncsu.edu>
2014-08-01 11:33:11 -0400

    fix to staff location field

--- templates/staff/views-view-field--staff--block-2--field-location-group.tpl.php
+++ templates/staff/views-view-field--staff--block-2--field-location-group.tpl.php
@@ -1,8 +1,6 @@
 <?php
-	$i = 0;
 	foreach($row->field_field_location_group as $item):
-		$i++;
-		$vals = $item['rendered']['entity']['field_collection_item'][$i];
+		$vals = reset($item['rendered']['entity']['field_collection_item']);
 		$building = $vals['field_staff_building']['#object']->field_staff_building['und'][0]['entity'];
 		$building_name = $building->title;
 		$building_address = $building->field_address['und'][0]['value'];
@@ -10,7 +8,7 @@
 ?>
 	<p>
 		<strong><?= $building_name; ?></strong><br/>
-		<?php if($office_number){ echo $office_number;} ?><br/>
+		<?php if($office_number){ echo $office_number.'<br/>';} ?>
 		<?= nl2br($building_address); ?><br/>
 	</p>
 
