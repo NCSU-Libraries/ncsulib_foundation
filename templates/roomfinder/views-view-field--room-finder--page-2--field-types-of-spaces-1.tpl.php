ddea4cdfdd560c625d28b4a4f5c3d7c199bf6152
Erik Olson <eolson@ncsu.edu>
2014-08-19 12:19:14 -0400

    new hi tech space field

--- templates/roomfinder/views-view-field--room-finder--page-2--field-types-of-spaces-1.tpl.php
+++ templates/roomfinder/views-view-field--room-finder--page-2--field-types-of-spaces-1.tpl.php
@@ -1,6 +1,6 @@
 <?php
     echo $output;
-    if (strpos($str, 'High-Tech Space') !== FALSE)
+    if (strpos($str, 'High-Tech Space') !== FALSE){
         echo '<p><i class="fa fa-cube"></i> High-Tech Space</p>';
     } else{
         return;
