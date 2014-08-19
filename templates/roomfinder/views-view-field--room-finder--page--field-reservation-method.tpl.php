568453f34206f3c9587392a2eaf96df844200076
Erik Olson <eolson@ncsu.edu>
2014-08-19 09:46:39 -0400

    move 'request res button to left

--- templates/roomfinder/views-view-field--room-finder--page--field-reservation-method.tpl.php
+++ templates/roomfinder/views-view-field--room-finder--page--field-reservation-method.tpl.php
@@ -5,7 +5,7 @@
           // Print out the url depending on whether or not there is a value
           // set for the field_request_form_url
           $reservation_url = isset($row->field_field_request_form_url) ? $row->field_field_request_form_url[0]['raw']['url'] :  '/huntlibrary/roomrequest';
-          print '<a class="button tiny right" href="' . $reservation_url . '">Request a reservation</a>';
+          print '<a class="button tiny" href="' . $reservation_url . '">Request a reservation</a>';
           break;
 
         case 'By Room Reservation System':
