cd0e7b1f09a81ab695e7af9d2d153e5b3f2ac0cc
Erik Olson <eolson@ncsu.edu>
2014-09-19 10:22:09 -0400

    adding placeholder for ie8

--- scripts/ie8.js
+++ scripts/ie8.js
@@ -1,7 +1,9 @@
 window.onload = function (){
-	thebody = document.getElementsByTagName('body')[0];
-	str = "<div id='browser_warning'><h4>The NCSU Library's website is not optimized for your browser.<br/> Would you consider using a <a href='http://browsehappy.com/'>modern browser</a>?</h4></div>";
-	thediv = document.createElement('div');
-	thediv.innerHTML = str;
-	thebody.insertBefore(thediv, thebody.firstChild);
+    thebody = document.getElementsByTagName('body')[0];
+    str = "<div id='browser_warning'><h4>The NCSU Library's website is not optimized for your browser.<br/> Would you consider using a <a href='http://browsehappy.com/'>modern browser</a>?</h4></div>";
+    thediv = document.createElement('div');
+    thediv.innerHTML = str;
+    thebody.insertBefore(thediv, thebody.firstChild);
+
+    $('input, textarea').placeholder(); //placeholder polyfill
 }
