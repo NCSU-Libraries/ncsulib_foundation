27cc1894c56b50e96f6d41f241eccac708da9133
Erik Olson <eolson@ncsu.edu>
2014-10-01 10:39:37 -0400

    changing ie 8 browser warning message

--- scripts/ie8.js
+++ scripts/ie8.js
@@ -1,7 +1,7 @@
 window.onload = function (){
     // add browser warning
     thebody = document.getElementsByTagName('body')[0];
-    str = "<div id='browser_warning'><h4>The NCSU Library's website is not optimized for your browser.</h4></div>";
+    str = "<div id='browser_warning'><h4>This site may not work well on Internet Explorer versions 8 or lower.</h4></div>";
     thediv = document.createElement('div');
     thediv.innerHTML = str;
     thebody.insertBefore(thediv, thebody.firstChild);
