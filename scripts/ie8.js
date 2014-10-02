039da91786427542839e46c744473404e168d282
Erik Olson <eolson@ncsu.edu>
2014-10-02 13:36:56 -0400

    fix for tabbing through site showing off canvas area

--- scripts/ie8.js
+++ scripts/ie8.js
@@ -1,7 +1,7 @@
 window.onload = function (){
     // add browser warning
     thebody = document.getElementsByTagName('body')[0];
-    str = "<div id='browser_warning'><h4>This site may not work well on Internet Explorer versions 8 or lower.</h4></div>";
+    str = "<div id='browser_warning'><h4>This site may not work well on Internet Explorer versions 8 or lower</h4></div>";
     thediv = document.createElement('div');
     thediv.innerHTML = str;
     thebody.insertBefore(thediv, thebody.firstChild);
