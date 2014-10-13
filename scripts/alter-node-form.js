5eb3c337ac297a783d72ca3a2dfdb13caedd1026
Erik Olson <eolson@ncsu.edu>
2014-10-13 15:00:51 -0400

    unsucessful node form alterations

--- scripts/alter-node-form.js
+++ scripts/alter-node-form.js
@@ -1,6 +1,6 @@
 var form = {
     init : function(){
-        jQuery('ul.tips').hide().before('<a href="#" id="tips-toggle">View formatting tips</a>');
+        jQuery('ul.tips').hide().before('<p><a href="#" id="tips-toggle">View formatting tips</a></p>');
         jQuery('#tips-toggle').click(function(e){
             jQuery('ul.tips').toggle();
 
