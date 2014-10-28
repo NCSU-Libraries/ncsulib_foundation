1055386fbf9a79f0dab9067a756358b379b68a5b
Erik Olson <eolson@ncsu.edu>
2014-10-28 11:59:39 -0400

    slight tweak

--- scripts/branding-bar-tracking.js
+++ scripts/branding-bar-tracking.js
@@ -2,35 +2,35 @@ var brandingTracking = {
     init : function(){
 
         jQuery('.ncstate-utility-bar-home').click(function(e){
-            _gaq.push(['_trackEvent', 'Branding Bar', 'NC State Home']);
             brandingTracking.pause();
+            _gaq.push(['_trackEvent', 'Branding Bar', 'NC State Home']);
         })
 
         jQuery('#ncstate-utility-bar-toggle-link').hover(function(e){
-            _gaq.push(['_trackEvent', 'Branding Bar', 'Resources', 'toggle button']);
             brandingTracking.pause();
+            _gaq.push(['_trackEvent', 'Branding Bar', 'Resources', 'toggle button']);
         })
 
         jQuery('#search-input').click(function(e){
-            _gaq.push(['_trackEvent', 'Branding Bar', 'Search NCSU']);
             brandingTracking.pause();
+            _gaq.push(['_trackEvent', 'Branding Bar', 'Search NCSU']);
         })
 
         jQuery('#search-submit').click(function(e){
-            _gaq.push(['_trackEvent', 'Branding Bar', 'Search NCSU']);
             brandingTracking.pause();
+            _gaq.push(['_trackEvent', 'Branding Bar', 'Search NCSU']);
         })
 
         jQuery('li.ncstate-utility-bar-primary-util a').click(function(e){
             var val = $(this).text();
-            _gaq.push(['_trackEvent', 'Branding Bar', 'Resources', val]);
             brandingTracking.pause();
+            _gaq.push(['_trackEvent', 'Branding Bar', 'Resources', val]);
         })
 
         jQuery('div.ncstate-utility-bar-sec-util li a').click(function(e){
             var val = $(this).text();
-            _gaq.push(['_trackEvent', 'Branding Bar', 'Resources', val]);
             brandingTracking.pause();
+            _gaq.push(['_trackEvent', 'Branding Bar', 'Resources', val]);
         })
     },
 
