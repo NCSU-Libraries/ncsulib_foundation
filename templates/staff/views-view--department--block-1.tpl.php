05b6d3b7df76075d070ee21ddf8bd08fd70889fd
Erik Olson <eolson@ncsu.edu>
2014-11-07 11:43:01 -0500

    change to dept sorting. use short name instead of title

--- templates/staff/views-view--department--block-1.tpl.php
+++ templates/staff/views-view--department--block-1.tpl.php
@@ -1,11 +1,12 @@
 <select id="department-select" onchange="window.location=this.options[this.selectedIndex].value">
-	<option value="">- Select a Department -</option>
-	<?php
+    <option value="">- Select a Department -</option>
+    <?php
 		foreach($view->result as $result):
 			$url = str_replace(' ', '-',$result->node_title);
+            $short_name = $result->field_field_department_short_name[0]['raw']['value'];
 	?>
 
-	<option value="/1staff/results?department=<?= $result->node_title; ?>"><?= $result->node_title; ?></option>
+	<option value="/1staff/results?department=<?= $short_name; ?>"><?= $result->node_title; ?></option>
 
 	<?php endforeach; ?>
 </select>
