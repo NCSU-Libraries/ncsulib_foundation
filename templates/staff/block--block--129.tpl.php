5a1d0c524c7488d96fe03cbdc875116fdd17dd60
Erik Olson <eolson@ncsu.edu>
2014-11-25 10:02:25 -0500

    sad and unused block

---
+++ templates/staff/block--block--129.tpl.php
@@ -0,0 +1,26 @@
+<h3><?= $block->title; ?></h3>
+
+<?php
+
+    $userID = arg(1);
+    $html = file_get_contents('http://webdev.lib.ncsu.edu/recommended/contacts.json');
+    $data = json_decode($html);
+    $user_data = $data->$userID->courses;
+
+    foreach($user_data as $key=>$course){
+    echo '<p>';
+        if($course[0] == 0){
+            echo '<a href="/course/'.strtoupper($key).'"><strong>'.strtoupper($key) . '</strong></a> ';
+        } else{
+            echo '<strong>' . strtoupper($key) . '</strong> ';
+        }
+        foreach($course as $key2=>$c){
+            if($c != 0){
+                echo '<a href="/course/'.strtoupper($key).'/'.$c.'">'.$c.'</a> ';
+            }
+        }
+    echo '</p>';
+    }
+
+
+?>
