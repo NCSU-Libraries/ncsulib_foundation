77f2e54eb838e5708840ad43213caf190a8f5dd4
Erik Olson <eolson@ncsu.edu>
2014-12-03 11:17:41 -0500

    if no data do not show

--- templates/staff/block--block--129.tpl.php
+++ templates/staff/block--block--129.tpl.php
@@ -1,25 +1,25 @@
-<h3><?= $block->title; ?></h3>
-
 <?php
 
     $userID = arg(1);
     $html = file_get_contents('http://webdev.lib.ncsu.edu/recommended/contacts.json');
     $data = json_decode($html);
     $user_data = $data->$userID->courses;
-
-    foreach($user_data as $key=>$course){
-    echo '<p>';
-        if($course[0] == 0){
-            echo '<a href="/course/'.strtoupper($key).'"><strong>'.strtoupper($key) . '</strong></a> ';
-        } else{
-            echo '<strong>' . strtoupper($key) . '</strong> ';
-        }
-        foreach($course as $key2=>$c){
-            if($c != 0){
-                echo '<a href="/course/'.strtoupper($key).'/'.$c.'">'.$c.'</a> ';
+    if(count($user_data)){
+        echo '<h3>'.$block->title.'</h3>';
+        foreach($user_data as $key=>$course){
+        echo '<p>';
+            if($course[0] == 0){
+                echo '<a href="/course/'.strtoupper($key).'"><strong>'.strtoupper($key) . '</strong></a> ';
+            } else{
+                echo '<strong>' . strtoupper($key) . '</strong> ';
             }
+            foreach($course as $key2=>$c){
+                if($c != 0){
+                    echo '<a href="/course/'.strtoupper($key).'/'.$c.'">'.$c.'</a> ';
+                }
+            }
+        echo '</p>';
         }
-    echo '</p>';
     }
 
 
