557db1f1b6283da7194dea8e10d883507b8d6054
Erik Olson <eolson@ncsu.edu>
2014-12-09 17:59:11 -0500

    removing errors

--- templates/includes/header.php
+++ templates/includes/header.php
@@ -12,7 +12,7 @@
 
   		// Boot drupal so t() functions and other drupal functions run
   		require_once (DRUPAL_ROOT.'/includes/bootstrap.inc');
-  		// drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+  		drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
   	}
 ?>
 
