a339b4dd4000bdce3d5b5b4df4b5b9165ba01a01
Erik Olson <eolson@ncsu.edu>
2014-12-22 11:45:19 -0500

    change util hours to show open to close hours range

--- templates/includes/partials/util-hours.php
+++ templates/includes/partials/util-hours.php
@@ -2,9 +2,9 @@
 	$ary = array();
 	$util_ary = array('hill'=>22493, 'hunt'=>22494);
 	foreach($util_ary as $key=>$lib){
-		$raw_json = file_get_contents($GLOBALS['base_url'].'/rest_hours/master-hours-feed.json?library='.$lib.'&service_short_name=general&date='.date('Y').'-'.date('n').'-'.date('j'));
+		$raw_json = file_get_contents($GLOBALS['base_url'].'/rest_hours/master-hours-feed.json?library='.$lib.'&service_short_name=general&date='.date('Y-n-j'));
 		$json = json_decode($raw_json);
-		$ary[$key] = $json[0]->real_time_display;
+		$ary[$key] = $json[0]->display;
 	}
 	return $ary;
 ?>
