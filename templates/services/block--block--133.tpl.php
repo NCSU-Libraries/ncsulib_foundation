fcaa7ad791a01ef136179b1271fb5732e735064f
Erik Olson <eolson@ncsu.edu>
2015-01-05 10:31:33 -0500

    makerspace hours

---
+++ templates/services/block--block--133.tpl.php
@@ -0,0 +1,17 @@
+<section class="block block-block contextual-links-region">
+<?php
+
+ $mkr_ary = hours_get_reg_schedule('hunt','makerspace');
+foreach($mkr_ary as $key => $hours){
+    echo '<h3>'.ucfirst(key($mkr_ary)) . ' Hours</h3>';
+    echo '<table>';
+    foreach($hours as $hour){
+        echo '<tr>';
+        echo '<td>'.$hour['day_range'].'</td>';
+        echo '<td>'.$hour['open_hours'].'</td>';
+        echo '</tr>';
+    }
+    echo '</table>';
+}
+?>
+</section>
