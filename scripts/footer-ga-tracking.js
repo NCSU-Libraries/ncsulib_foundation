556819353cdc3d658a13585e822761968d9d95ab
Erik Olson <eolson@ncsu.edu>
2015-01-14 14:46:16 -0500

    adding universal analytics tracking for footer

--- scripts/footer-ga-tracking.js
+++ scripts/footer-ga-tracking.js
@@ -7,24 +7,29 @@ var ga_footer = {
 		// contact
 		jQuery('#f-contact-hill').click(function(e){
 			_gaq.push(['_trackEvent', 'Footer', 'Contact Us', 'Hill']);
+			ga('send', 'event', 'Footer(new)', 'Contact Us(new)', 'Hill(new)');
 			ga_footer.pause();
 		})
 		jQuery('#f-hill-phone').click(function(e){
 			_gaq.push(['_trackEvent', 'Footer', 'Contact Us', 'Hill Phone']);
+			ga('send', 'event', 'Footer(new)', 'Contact Us(new)', 'Hill Phone(new)');
 			ga_footer.pause();
 		})
 		jQuery('#f-contact-hunt').click(function(e){
 			_gaq.push(['_trackEvent', 'Footer', 'Contact Us', 'Hunt']);
+			ga('send', 'event', 'Footer(new)', 'Contact Us(new)', 'Hunt(new)');
 			ga_footer.pause();
 		})
 		jQuery('#f-hunt-phone').click(function(e){
 			_gaq.push(['_trackEvent', 'Footer', 'Contact Us', 'Hunt Phone']);
+			ga('send', 'event', 'Footer(new)', 'Contact Us(new)', 'Hunt Phone(new)');
 			ga_footer.pause();
 		})
 
 		// libraries
 		jQuery('#f-libraries h2 a').click(function(e){
 			_gaq.push(['_trackEvent', 'Footer', 'Libraries']);
+			ga('send', 'event', 'Footer(new)', 'Libraries(new)');
 			ga_footer.pause();
 		})
 
@@ -32,6 +37,7 @@ var ga_footer = {
 		jQuery('ul#f-lib-links li a').click(function(e){
 			var lib = jQuery(this).text();
 			_gaq.push(['_trackEvent', 'Footer', 'Libraries', lib]);
+			ga('send', 'event', 'Footer(new)', 'Libraries(new)' lib+'(new)');
 			ga_footer.pause();
 		})
 
@@ -39,6 +45,7 @@ var ga_footer = {
 		jQuery('ul#f-util-links li a').click(function(e){
 			var link = jQuery(this).text();
 			_gaq.push(['_trackEvent', 'Footer', 'Utility', link]);
+			ga('send', 'event', 'Footer(new)', 'Utility(new)' lib+'(new)');
 			ga_footer.pause();
 		})
 
@@ -46,12 +53,14 @@ var ga_footer = {
 		jQuery('#f-social ul li a').click(function(e){
 			link = jQuery(this).text();
 			_gaq.push(['_trackEvent', 'Footer', 'Social', link]);
+			ga('send', 'event', 'Footer(new)', 'Social(new)' link+'(new)');
 			ga_footer.pause();
 		})
 
 		// giving
 		jQuery('.giving').click(function(e){
 			_gaq.push(['_trackEvent', 'Footer', 'Giving']);
+			ga('send', 'event', 'Footer(new)', 'Giving(new)';
 			ga_footer.pause();
 		})
 
@@ -59,6 +68,7 @@ var ga_footer = {
 		jQuery('.feedback-form').click(function(e){
 			link = jQuery(this).text();
 			_gaq.push(['_trackEvent', 'Footer', 'Feedback']);
+			ga('send', 'event', 'Footer(new)', 'Feedback(new)';
 			ga_footer.pause();
 		})
 	},
