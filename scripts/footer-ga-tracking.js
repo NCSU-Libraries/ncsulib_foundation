c26ea9df6e4f53b715c316c0ac3605093a5abb6f
Erik Olson <eolson@ncsu.edu>
2015-01-14 14:56:24 -0500

    refix for GA tracking. remove all old code for new style

--- scripts/footer-ga-tracking.js
+++ scripts/footer-ga-tracking.js
@@ -6,69 +6,69 @@ var ga_footer = {
 	init : function(){
 		// contact
 		jQuery('#f-contact-hill').click(function(e){
-			_gaq.push(['_trackEvent', 'Footer', 'Contact Us', 'Hill']);
-			ga('send', 'event', 'Footer(new)', 'Contact Us(new)', 'Hill(new)');
+			// _gaq.push(['_trackEvent', 'Footer', 'Contact Us', 'Hill']);
+			ga('send', 'event', 'Footer', 'Contact Us', 'Hill');
 			ga_footer.pause();
 		})
 		jQuery('#f-hill-phone').click(function(e){
-			_gaq.push(['_trackEvent', 'Footer', 'Contact Us', 'Hill Phone']);
-			ga('send', 'event', 'Footer(new)', 'Contact Us(new)', 'Hill Phone(new)');
+			// _gaq.push(['_trackEvent', 'Footer', 'Contact Us', 'Hill Phone']);
+			ga('send', 'event', 'Footer', 'Contact Us', 'Hill Phone');
 			ga_footer.pause();
 		})
 		jQuery('#f-contact-hunt').click(function(e){
-			_gaq.push(['_trackEvent', 'Footer', 'Contact Us', 'Hunt']);
-			ga('send', 'event', 'Footer(new)', 'Contact Us(new)', 'Hunt(new)');
+			// _gaq.push(['_trackEvent', 'Footer', 'Contact Us', 'Hunt']);
+			ga('send', 'event', 'Footer', 'Contact Us', 'Hunt');
 			ga_footer.pause();
 		})
 		jQuery('#f-hunt-phone').click(function(e){
-			_gaq.push(['_trackEvent', 'Footer', 'Contact Us', 'Hunt Phone']);
-			ga('send', 'event', 'Footer(new)', 'Contact Us(new)', 'Hunt Phone(new)');
+			// _gaq.push(['_trackEvent', 'Footer', 'Contact Us', 'Hunt Phone']);
+			ga('send', 'event', 'Footer', 'Contact Us', 'Hunt Phone');
 			ga_footer.pause();
 		})
 
 		// libraries
 		jQuery('#f-libraries h2 a').click(function(e){
-			_gaq.push(['_trackEvent', 'Footer', 'Libraries']);
-			ga('send', 'event', 'Footer(new)', 'Libraries(new)');
+			// _gaq.push(['_trackEvent', 'Footer', 'Libraries']);
+			ga('send', 'event', 'Footer', 'Libraries');
 			ga_footer.pause();
 		})
 
 		//Library links
 		jQuery('ul#f-lib-links li a').click(function(e){
 			var lib = jQuery(this).text();
-			_gaq.push(['_trackEvent', 'Footer', 'Libraries', lib]);
-			ga('send', 'event', 'Footer(new)', 'Libraries(new)' lib+'(new)');
+			// _gaq.push(['_trackEvent', 'Footer', 'Libraries', lib]);
+			ga('send', 'event', 'Footer', 'Libraries' lib);
 			ga_footer.pause();
 		})
 
 		// util links
 		jQuery('ul#f-util-links li a').click(function(e){
 			var link = jQuery(this).text();
-			_gaq.push(['_trackEvent', 'Footer', 'Utility', link]);
-			ga('send', 'event', 'Footer(new)', 'Utility(new)' lib+'(new)');
+			// _gaq.push(['_trackEvent', 'Footer', 'Utility', link]);
+			ga('send', 'event', 'Footer', 'Utility' lib);
 			ga_footer.pause();
 		})
 
 		// social
 		jQuery('#f-social ul li a').click(function(e){
 			link = jQuery(this).text();
-			_gaq.push(['_trackEvent', 'Footer', 'Social', link]);
-			ga('send', 'event', 'Footer(new)', 'Social(new)' link+'(new)');
+			// _gaq.push(['_trackEvent', 'Footer', 'Social', link]);
+			ga('send', 'event', 'Footer', 'Social' link);
 			ga_footer.pause();
 		})
 
 		// giving
 		jQuery('.giving').click(function(e){
-			_gaq.push(['_trackEvent', 'Footer', 'Giving']);
-			ga('send', 'event', 'Footer(new)', 'Giving(new)';
+			// _gaq.push(['_trackEvent', 'Footer', 'Giving']);
+			ga('send', 'event', 'Footer', 'Giving');
 			ga_footer.pause();
 		})
 
 		// feedback
 		jQuery('.feedback-form').click(function(e){
 			link = jQuery(this).text();
-			_gaq.push(['_trackEvent', 'Footer', 'Feedback']);
-			ga('send', 'event', 'Footer(new)', 'Feedback(new)';
+			// _gaq.push(['_trackEvent', 'Footer', 'Feedback']);
+			ga('send', 'event', 'Footer', 'Feedback');
 			ga_footer.pause();
 		})
 	},
