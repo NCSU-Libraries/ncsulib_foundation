9c7f6d5820c8fbc9a31a8107657442e2d49c315d
Erik Olson <eolson@ncsu.edu>
2015-01-16 16:32:20 -0500

    adding special message for closed mini theater

--- templates/roomfinder/views-view-field--room-finder--page--field-reservation-method.tpl.php
+++ templates/roomfinder/views-view-field--room-finder--page--field-reservation-method.tpl.php
@@ -1,7 +1,7 @@
 <?php
   if (!empty($output)){
       switch ($output) {
-          case 'By Mediated Email Form':
+        case 'By Mediated Email Form':
           // Print out the url depending on whether or not there is a value
           // set for the field_request_form_url
           $reservation_url = isset($row->field_field_request_form_url) ? $row->field_field_request_form_url[0]['raw']['url'] :  '/huntlibrary/roomrequest';
@@ -14,6 +14,9 @@
           $schedule_id = $row->_field_data['nid']['entity']->field_room_res_id['und'][0]['value'];
           $output = '<a class="button" href="//www.lib.ncsu.edu/roomreservations/schedule.php?date='. $today .'&scheduleid='. $schedule_id .'">Reserve</a>';
           print $output;
+          break;
+        case 'Temporarily Unreservable':
+          echo '<em class="highlight">The Mini-Theater will be closed from February 9 through the end of the Spring Semester while renovations take place in the West Wing.</em>';
           break;
       }
     }
