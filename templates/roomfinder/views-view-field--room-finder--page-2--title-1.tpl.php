d4d51be7eebb2a93765f0e3cb8d92b00ba3315d9
Erik Olson <eolson@ncsu.edu>
2015-01-16 21:24:24 -0500

    fix formatting for message

--- templates/roomfinder/views-view-field--room-finder--page-2--title-1.tpl.php
+++ templates/roomfinder/views-view-field--room-finder--page-2--title-1.tpl.php
@@ -2,7 +2,7 @@
   if (!empty($output)){
       switch ($output) {
         case 'Mini-Theater':
-          echo '<em class="highlight">The Mini-Theater will be closed from February 9 through the end of the Spring Semester while renovations take place in the West Wing.</em>';
+          echo '<p><em class="highlight">The Mini-Theater will be closed from February 9 through the end of the Spring Semester while renovations take place in the West Wing.</em></p>';
           break;
       }
     }
