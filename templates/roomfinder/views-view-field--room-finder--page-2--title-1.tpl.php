dc6fe7abcd62f5f2e481851617c886e2f17493f7
Erik Olson <eolson@ncsu.edu>
2015-02-13 14:09:22 -0500

    reserve a room fix

--- templates/roomfinder/views-view-field--room-finder--page-2--title-1.tpl.php
+++ templates/roomfinder/views-view-field--room-finder--page-2--title-1.tpl.php
@@ -2,7 +2,7 @@
   if (!empty($output)){
       switch ($output) {
         case 'Mini-Theater':
-          echo '<p><em class="highlight">The Mini-Theater will be closed from February 9 through the end of the Spring Semester while renovations take place in the West Wing.</em></p>';
+          echo '<p><em class="highlight">The Mini-Theater is closed from February 9 through the end of the Spring Semester while renovations take place in the West Wing.</em></p>';
           break;
       }
     }
