a6422c3d747f00d92d94621bc279a15e8088bf99
Erik Olson <eolson@ncsu.edu>
2015-02-19 12:49:10 -0500

    fixing chat to include ncknows

--- templates/includes/utility-links.php
+++ templates/includes/utility-links.php
@@ -4,5 +4,5 @@
 	<li><a href="//www.lib.ncsu.edu/hours">HOURS</a></li>
 	<li><a href="//www.lib.ncsu.edu/faq/">FAQ</a></li>
     <li><a href="https://www.lib.ncsu.edu/website/logout.php">LOG OUT</a></li>
-	<li><a href="https://www.lib.ncsu.edu/contact" class="chat-link">CONTACT US</a></li>
+	<li><a href="" class="chat-link">CHAT NOW</a></li>
 <ul>
