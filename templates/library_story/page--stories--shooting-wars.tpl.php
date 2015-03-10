c9c99bd50989a1bfcf1e3ff991e26538f18a0ba7
Erik Olson <eolson@ncsu.edu>
2015-03-10 16:33:56 -0400

    new elevated story template

---
+++ templates/library_story/page--stories--shooting-wars.tpl.php
@@ -0,0 +1,133 @@
+<div id="content" role="document" class="page elevated-story">
+    <div id="feature-area"><img src="/sites/default/files/shooting_wars/shooting-wars-feature.jpg" width="100%" alt=""></div>
+    <main role="main" class="row l-main">
+        <div class="medium-8 medium-offset-2 columns">
+            <h1>Shooting Wars</h1>
+
+            <h2>PART 1: The Need</h2>
+
+            <p>As Professor Marsha Gordon, in December 2013, walked through the gates of the library into the heart of the building and stood in the Immersion Theater she turned to me and said “We have to do the project here.” It was her first visit to the Hunt Library. </p>
+
+            <p>Professor Gordon is an associate professor of film studies in the English Department at North Carolina State University, and over lunch a week earlier, she told me that maybe I - and the Libraries - could help her with her spring semester class on War Documentaries. Professor Gordon, like many talented NC State faculty, was considering how to make her next semester’s class more engaging, memorable, and challenging, but also more satisfying. She wanted to develop a final project that got her students to take what they were studying and make it their own, to create something new with it. “I want to do something big and visual,” she said.</p>
+
+            <div class="photo-cont">
+                <img src="/sites/default/files/shooting_wars/photo-5.jpg" width="100%" alt="">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+
+            <p>“What I originally wanted to do was to use the resources of this amazing library in an innovative way and to do a project the likes of which I had never done before,” said Professor Gordon two months after the project, <em>Shooting Wars: Documentary Images of American Military Conflicts</em>, debuted in the Hunt Library. "I really wanted to push my own limitations as a teacher and what I imagine doing in the classroom and to learn from that."</p>
+
+            <p>Her reaction upon seeing the Immersion Theater was exactly what NCSU librarians had imagined when designing the space. In it, she saw potential. Some version of this big, visual project was taking shape in her mind, and now it was up to us to work together to find it wherever it was and make it real.</p>
+
+            <div class="video-wrapper">
+                <iframe src="https://player.vimeo.com/video/121598191" width="100%" height="auto"  frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> <p><a href="https://vimeo.com/121598191">The Class</a> from <a href="https://vimeo.com/ncsulibraries">NCSU Libraries</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
+            </div>
+
+            <p>In early February, Professor Gordon and the students in her English 585: War Documentaries class met me at the Hunt Library. I initially planned to give the class a brief tour of two of the library’s high tech visualization spaces: the Creativity Studio and the Teaching and Visualization Lab. Until shortly before this meeting, however, I had failed to consider the GameLab, an intimate room dominated by a 20’ x 5’ MicroTiles video display wall, as a place to exhibit the final projects. But when I thought about the privacy offered by the room’s fogging windows (intended for developers working with sensitive code), it hit me: This was the perfect place to do a visual exhibit of challenging, and sometimes disturbing, war imagery. Our tour of the Hunt Library started - and stopped - in the GameLab.</p>
+
+            <h2>PART 2: Collaboration</h2>
+
+            <p>I frequently give tours of the Hunt Library, and as we walked into the Game Lab, I recited the space’s features – inputs that can cascade up to eight different video signals across the video wall, a powerful audio system, the ability to allow users to interact with the MicroTiles wall like a touchscreen, and the <em>Blade Runner</em>-esque fog controls for the windows. As they listened and looked around the space, it was apparent that Professor Gordon and her students were taken with the room and saw its many possibilities. Brian Robertson, a student in the class, felt the appropriateness of the GameLab immediately. “There were obviously questions of sensitivity due to the subject matter but the fact that it was public, and had the windows that could be fogged if necessary was great. It was pretty clear early on that it was going to be an ideal space for what we needed to do.”</p>
+
+
+            <p>What happened after this visit to the Hunt Library was like nothing in my career as an NC State librarian I had experienced before. Gordon allowed her students, me, and the GameLab itself shape and define the final project. Student Jason Buel said, “The library space was pretty crucial in the sense that it offered not only a place to do the project but a place to kind of figure out what the project was gonna be, before we really knew that.”</p>
+
+            <div class="video-wrapper">
+                <iframe src="https://player.vimeo.com/video/121598190" width="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> <p><a href="https://vimeo.com/121598190">The GameLab</a> from <a href="https://vimeo.com/ncsulibraries">NCSU Libraries</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
+            </div>
+
+            <p>Professor Gordon: “We really started talking about, ‘OK, what are the kind of technological things that need to happen? We need to be able to digitize bits and pieces of media to use … and we need to be able to have text. So what are the appropriate technological aspects of this?’”</p>
+
+            <p>In the end, everyone agreed that the project should take the form of a visual exhibit. Each exhibit piece would focus on an American military conflict (e.g. World War II; the Korean Conflict; the War on Terror) and play multiple videos of documentary footage from that conflict side-by-side on the screen, with a single soundtrack running over them. “We had three layered tracks” said Professor Gordon, “image, sound, and text. Each panel had images, had text, and of course had sound that could have been related to it or completely divorced from it.” “Each of us in the class took a war in which Americans had been involved,” said Brian Robertson. “We tried to find footage from documentaries or from just raw footage that dealt with images of technologies in those wars, dealt with images of human suffering, and particularly death and try to deal with them in a way that commented on the images.”</p>
+
+            <div class="video-wrapper">
+                <iframe src="https://player.vimeo.com/video/121598187" width="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> <p><a href="https://vimeo.com/121598187">The Project</a> from <a href="https://vimeo.com/ncsulibraries">NCSU Libraries</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
+            </div>
+
+            <p>Noting that the class was not a production class, and that she was asking quite a bit of her students for this project, Professor Gordon decided to create an exhibit piece herself. This very meaningful move, brought the class together in a very palpable way. Professor Gordon now had as much to gain from the class as her students did, could empathize with their material and technical struggles, and would also be contributing – and paying attention – to the final outcome in a way that is very different from a professor simply grading papers or tests. Jason Buel said, “You’ve got everybody working on essentially three different films: One that shows representations of death, one that shows representations of technology, and one that shows text to kind of orient the viewer. Putting those three separate films together to have … three separate frames on a single screen, and then editing those together to produce this overall longer work.”</p>
+
+            <p>All of these exciting ideas were tempered by some very real challenges. [Still need a little transitional glue here.] The Hunt Library was only a year old and nobody had attempted something quite like this in the GameLab before. I was operating on what I knew was possible instead of what had been proven. But Professor Gordon and her students were in the same boat. Very few of them created video projects before, let alone screens of video within video on 20’ wide displays. For all of us, the challenge was energizing and exciting.</p>
+
+            <blockquote>
+                <p>“Jason Evans Groth put together a fantastic PowerPoint based application for [the] ginormous monitor … in the GameLab at Hunt Library. He got the layout put together and that’s when everything got real.”</p>
+            </blockquote>
+
+            <p>To actually make this project happen, Professor Gordon and her students played the roles of content creators and project directors. They told me what they wanted the final projects to look like. I served the role of intermediary between them and the GameLab, providing content guidelines and technical requirements and limitations. I created templates and tutorials to guide and assist them, and made myself as available as possible to help them.</p>
+
+            <div class="video-wrapper">
+                <iframe src="https://player.vimeo.com/video/121597398" width="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> <p><a href="https://vimeo.com/121597398">Shooting Wars - Production</a> from <a href="https://vimeo.com/ncsulibraries">NCSU Libraries</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
+            </div>
+
+            <h2>PART 3: The Outcome</h2>
+
+            <p>As Professor Gordon and the students sent me their videos to test on the GameLab wall, I knew we had done something very exciting. We had built something completely new and it was going to work. We used this this high tech environment to render the technology as invisible as possible so the audience might engage with this powerful exhibit content in a meaningful way. I couldn’t wait for the class to see it.</p>
+
+            <p>On April 15, the class arrived at the GameLab to prepare for the exhibit’s opening that evening. None of the students had seen their work on the giant screen, nor had they seen one another’s final products. </p>
+
+            <blockquote>
+                <p>“Across the board I think everyone was really ecstatic about the way that it turned out”</p>
+                <span class="ref">- Marsha Gordon</span>
+            </blockquote>
+
+            <blockquote>
+                <p>“It was really amazing to see everything all together and see the way that everything finally did come together”</p>
+                <span class="ref">- Jason Buel</span>
+            </blockquote>
+
+            <blockquote>
+                <p>“I was really pleased … [and] I was really impressed with everybody else’s [projects]”</p>
+                <span class="ref">- Brian Robertson</span>
+            </blockquote>
+
+            <p>Each exhibit piece spanned all 20’ of the MicroTiles video wall, running three videos side by side. In each piece, the video on the left showed images of technology associated with the chosen conflict; the video on the right showed images of death from that conflict; and the center video bridged the two with text or other analysis, either original words by the creator of the piece or collections of poems, quotes, or other research strung together in a new way. </p>
+
+            <p><img src="/sites/default/files/shooting_wars/world_wars.jpg" width="100%" alt=""></p>
+
+            <p>Each individual exhibit piece was brief, running three minutes or less. And a viewer could select a project from a menu by touching the screen.</p>
+
+            <p><img src="/sites/default/files/shooting_wars/shooting-wars2.jpg" width="100%" alt=""></p>
+
+            <p>The interactive component would entail empowering the viewer to decide which video they would watch and could start or stop those videos as they chose by selecting them on the big screen, creating the possibility of serendipitous meaning-making depending on how many videos, and in which order, the viewer chose to watch.</p>
+
+            <p>At the public opening on April 15, the students spoke to guests about their work and showed their final video projects in full, and on two subsequent days class members would be on hand to act as docents for the exhibit for any passers by. </p>
+
+            <p>When the opening was over, the exhibit was left on as visitors stepped in and out, choosing the videos for which they had the most interest, interacting with the screen and the artists, and the mood in the room was of both great relief and great satisfaction. As the week progressed, around 120 visitors - students, faculty, and staff of NCSU, and the general public - came to interact with the exhibit and with the artists behind it. </p>
+
+            <p>When the dust settled it all seemed so much more important and longer lasting than it did while we were simply trying to figure out how to get videos to play on demand on the touch screen. We had all accomplished something none of us had ever tried to do before. The class learned professional video editing techniques while utilizing traditional research methods using media, and were able to capitalize on the amazing visualization spaces inside the Hunt Library for a purpose not expressly clarified in its creation. And we all learned from one another during the process – learned to love this kind of work, learned to love working with one another, and learned about a whole new way to imagine what kind of learning these spaces actually have to offer. </p>
+
+            <blockquote>
+                <p>“Without that group of people from all over different parts of the university this project probably wouldn’t have come together”</p>
+                <span class="ref">- Kenneth Pinion</span>
+            </blockquote>
+
+            <blockquote>
+                <p>“A nice mixture of independence and collaboration … it gave me a lot of appreciation for the hands on aspect of teaching … doing a project like this and knowing that people were gonna see it outside of that class, that it was for more than a grade, that it might affect someone outside of what can feel like a pretty insular world,”</p>
+                <span class="ref">- Brian Robertson</span>
+            </blockquote>
+
+            <blockquote>
+                <p>“When you’ve got a projec that’s interesting, that’s generating attention and that other people find worthwhile, opportunities for collaboration just pop up out of nowhere … I’ve never really thought of the library as a space for exhibition and distribution of these kinds of works, because it’s kind of new, it’s not really an idea that’s been around all that long.”</p>
+                <span class="ref">- Jason Buel</span>
+            </blockquote>
+
+            <blockquote>
+                <p>“I think that is going to be increasingly important for students and for faculty to be able to think about creating in a way that’s not entirely academic and not entirely non-academic at the same time … I think that collaboration between students, librarians, and faculty … is one of the things that I think will define the 21st Century Academic Experience”</p>
+                <span class="ref">- Professor Gordon</span>
+            </blockquote>
+
+            <p><em>Shooting Wars</em> iswas a reminder of how important collaborations between libraries and classes are. The distinctions between librarian and student and professor were blurred to the point where we simply became collaborators, problem solvers working on and interpreting the outcome of something that we, ourselves, created. Not only did we blaze a trail for other projects like this, we proved they could be done by non-experts who were enthusiastic and persistent, who relied on other experts for guidance, who backed up their belief with hard work, and who understood that the process was just as important as the creation.</p>
+
+            <blockquote>
+                <p>“At the end of the day, that’s what a project like <em>Shooting Wars</em> means to me, that this is a kind of future trajectory of what we can do in our classrooms between students, librarians, and the broader community - think about how to use these spaces to create and disseminate knowledge in new ways, and to talk to each other in ways that are maybe more open and more performative than I certainly have in my classes in the past.”</p>
+                <span class="ref">- Professor Gordon</span>
+            </blockquote>
+
+            <div class="video-wrapper">
+                <iframe src="https://player.vimeo.com/video/121598188" width="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> <p><a href="https://vimeo.com/121598188">The Outcome</a> from <a href="https://vimeo.com/ncsulibraries">NCSU Libraries</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
+            </div>
+
+            <p>SEE THE VIRTUAL EXHIBIT HERE at <a href="http://www.lib.ncsu.edu/display/transfer/shooting_wars/">http://www.lib.ncsu.edu/display/transfer/shooting_wars/</a></p>
+        </div>
+    </main>
+</div>
