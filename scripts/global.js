31398141e76bc7d58de1145e1984574aa85c9953
Erik Olson <eolson@ncsu.edu>
2015-03-12 21:02:57 -0400

    iframe respinsiveness

--- scripts/global.js
+++ scripts/global.js
@@ -7,6 +7,7 @@
 jQuery(function(){
 	// check for orbit slider
 	slider_check();
+    iframeCheck();
 })
 
 
@@ -17,6 +18,16 @@ function slider_check(){
 
         jQuery('.orbit-slider').attr('data-orbit', '').attr('data-options', 'animation:fade;slide_number:false;animation_speed:300;timer_speed:5000;');
 
+    }
+}
+
+function iframeCheck(){
+    var elem = $('#main-content iframe');
+    if ($(elem).length > 0) {
+        var width = $(elem).attr('width');
+        var widVal = width.split('%');
+        $(elem).wrap( "<div class='video-container'></div>").css({'width':width,'height':width});
+        $(elem).parent().css('margin-bottom' , (-(widVal[0]/2))+'%');
     }
 }
 
