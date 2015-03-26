6685a3c16af0bfa30d6259bd2c3f245ccfaf8dfb
Erik Olson <eolson@ncsu.edu>
2015-03-26 13:07:09 -0400

    add static brand bar

---
+++ templates/includes/ncsu-brand-bar.php
@@ -0,0 +1,91 @@
+<link rel="stylesheet" type="text/css" href="/sites/all/themes/ncsulib_foundation/styles/core/custom/main-red.css">
+<div class="ncstate-utility-bar" role="navigation" aria-label="NC State Brand Utility Bar">
+    <div class="ncstate-utility-bar-tools">
+        <div id="ncstate-utility-bar-wrapper-primary" class="ncstate-utility-bar-wrapper ncstate-utility-bar-wrapper-primary">
+            <div class="ncstate-utility-bar-home">
+                <a href="http://www.ncsu.edu">NC State Home</a>
+            </div>
+            <div class="ncstate-utility-bar-options">
+                <div class="ncstate-utility-bar-toggle">
+                    <a href="#" aria-label="Toggle resources" id="ncstate-utility-bar-toggle-link">Resources</a>
+                    <div class="indicator" style="display: none;"></div>
+                </div>
+                <div aria-label="search ncsu.edu" role="search" class="ncstate-utility-bar-search">
+                    <form class="ncstate-utility-bar-search-form" name="navsearchform" action="https://www.ncsu.edu/search">
+                        <input type="hidden" value="undefined" name="cx" id="navcx">
+                        <input type="search" aria-label="search ncsu.edu" placeholder="search ncsu.edu" name="q" id="search-input" class="ncstate-utility-bar-search-field" style="padding-right: 23px; float: left;">
+                        <input type="submit" class="ncstate-utility-bar-search-submit" value="search" name="search-submit" id="search-submit">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="ncstate-utility-bar-links is-hidden" style="display: none;">
+        <div class="ncstate-utility-bar-wrapper">
+            <ul class="ncstate-utility-bar-primary-util">
+                <li class="ncstate-utility-bar-primary-util ncstate-utility-bar-directory">
+                    <a id="ncstate-utility-bar-first-link" href="http://www.ncsu.edu/directory/">Campus Directory</a>
+                </li>
+                <li class="ncstate-utility-bar-primary-util ncstate-utility-bar-mypack">
+                    <a href="http://mypack.ncsu.edu/">MyPack Portal</a>
+                </li>
+                <li class="ncstate-utility-bar-primary-util ncstate-utility-bar-map">
+                    <a href="http://maps.ncsu.edu/">Campus Map</a>
+                </li>
+                <li class="ncstate-utility-bar-primary-util ncstate-utility-bar-libraries">
+                    <a href="http://www.lib.ncsu.edu/">Libraries</a>
+                </li>
+            </ul>
+            <div class="ncstate-utility-bar-sec-util">
+                <dl>
+                    <dt>Campus Resources</dt>
+                    <dd>
+                        <ul>
+                            <li><a href="http://housing.ncsu.edu/">University Housing</a></li>
+                            <li><a href="http://www.ncsudining.com/">University Dining</a></li>
+                            <li><a href="http://shop.bookstore.ncsu.edu/">Bookstores</a></li>
+                            <li><a href="http://ncsu.transloc.com/">Wolfline Buses</a></li>
+                            <li><a href="http://www.ncsu.edu/calendars">Calendar</a></li>
+                        </ul>
+                    </dd>
+                </dl>
+                <dl>
+                    <dt>Academic Resources</dt>
+                    <dd>
+                        <ul>
+                            <li><a href="http://admissions.ncsu.edu/">Undergraduate Admissions</a></li>
+                            <li><a href="http://www.ncsu.edu/academics/departments-a-z/">Academic Departments</a></li>
+                            <li><a href="https://wolfware.ncsu.edu/">WolfWare</a></li>
+                            <li><a href="http://oia.ncsu.edu/">International Affairs</a></li>
+                            <li><a href="http://distance.ncsu.edu/">Distance Education</a></li>
+                        </ul>
+                    </dd>
+                </dl>
+                <dl>
+                    <dt>Other Resources</dt>
+                    <dd>
+                        <ul>
+                            <li><a href="http://www.fis.ncsu.edu/cashier/">Cashier's Office</a></li>
+                            <li><a href="http://www.ncsu.edu/registrar/">Registration and Records</a></li>
+                            <li><a href="http://financialaid.ncsu.edu/">Scholarships and Financial Aid</a></li>
+                            <li><a href="http://oe.ncsu.edu/">Outreach and Engagement</a></li>
+                            <li><a href="http://help.ncsu.edu/">NC State Help Desk</a></li>
+                        </ul>
+                    </dd>
+                </dl>
+                <dl>
+                    <dt>&nbsp;</dt>
+                    <dd>
+                        <ul>
+                            <li><a href="http://giving.ncsu.edu/">Giving</a></li>
+                            <li><a href="http://news.ncsu.edu/">NC State News</a></li>
+                            <li><a href="http://www.ncsu.edu/grad/">Graduate School</a></li>
+                            <li><a href="http://gmail.ncsu.edu/">Gmail Access</a></li>
+                        </ul>
+                    </dd>
+                </dl>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="/sites/all/themes/ncsulib_foundation/scripts/brand-bar.js?maxWidth=1000" type="text/javascript" charset="utf-8"></script>
