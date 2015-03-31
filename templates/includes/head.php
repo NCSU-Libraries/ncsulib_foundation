b5a9f7722c7ff89d24d099327a3a8e825fd8f749
Erik Olson <eolson@ncsu.edu>
2015-03-31 15:45:08 -0400

    universal ga for htdocs

--- templates/includes/head.php
+++ templates/includes/head.php
@@ -32,7 +32,7 @@
 </script>
 
 <!-- OLD Google Analytics -->
-<script type="text/javascript">
+<!-- <script type="text/javascript">
 
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-17138302-1']);
@@ -45,10 +45,10 @@
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
 
-</script>
+</script> -->
 
 <!-- NEW Google Analytics -->
-<!-- <script>
+<script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -57,7 +57,7 @@
   ga('create', 'UA-17138302-1', 'auto');
   ga('send', 'pageview');
 
-</script> -->
+</script>
 <!-- End Google Analytics -->
 
 <?php else: ?>
