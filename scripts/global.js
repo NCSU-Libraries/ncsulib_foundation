1e39a03080c9cd9246ddbdd4e4dd9b303d634f6b
Erik Olson <eolson@ncsu.edu>
2015-04-02 12:08:44 -0400

    make initial orbit slider dot active

--- scripts/global.js
+++ scripts/global.js
@@ -17,11 +17,10 @@ jQuery(function(){
 
 
 function slider_check(){
-
     if(jQuery('.orbit-slider').length > 0 && jQuery('.orbit-slider').attr('data-orbit') != ''){
-
         jQuery('.orbit-slider').attr('data-orbit', '').attr('data-options', 'animation:fade;slide_number:false;animation_speed:300;timer_speed:5000;');
-
+    } else if(jQuery('.orbit-slider').length > 0){
+        jQuery('.orbit-bullets li:first').addClass('active');
     }
 }
 
