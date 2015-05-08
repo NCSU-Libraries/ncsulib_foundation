3a3773cce2a36fd5932d3d61dd3f2e3a74550655
Erik Olson <eolson@ncsu.edu>
2015-05-08 14:10:23 -0400

    hours fix

--- templates/services/block--block--133.tpl.php
+++ templates/services/block--block--133.tpl.php
@@ -2,19 +2,19 @@
 <?php
 
  $mkr_ary = hours_get_reg_schedule('hunt','makerspace');
-foreach($mkr_ary as $key => $hours){
-    echo '<h2>'.key($mkr_ary) . ' Hour</h2>';
-    // kpr($key);
-    // echo '<h2>Hours</h2>';
-    echo '<table>';
-    foreach($hours as $hour){
-        echo '<tr>';
-        echo '<td>'.$hour['day_range'].'</td>';
-        echo '<td>'.$hour['open_hours'].'</td>';
-        echo '</tr>';
+    foreach($mkr_ary as $key => $hours){
+        echo '<h2>'.key($mkr_ary) . ' Hours</h2>';
+        // kpr($key);
+        // echo '<h2>Hours</h2>';
+        echo '<table>';
+        foreach($hours as $hour){
+            echo '<tr>';
+            echo '<td>'.$hour['day_range'].'</td>';
+            echo '<td>'.$hour['open_hours'].'</td>';
+            echo '</tr>';
+        }
+        echo '</table>';
     }
-    echo '</table>';
-}
 ?>
     <p><a href="/hours/hunt/makerspace">Full hours and exceptions</a></p>
 </section>
