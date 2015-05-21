185df54b45d450e4f8d8090278502822f2272a86
Erik Olson <eolson@ncsu.edu>
2015-05-21 11:54:39 -0400

    reposition cal popup

--- scripts/hours.js
+++ scripts/hours.js
@@ -3,13 +3,14 @@ jQuery(document).ready(function($){
 		if(!$(this).hasClass('empty') && !$(this).hasClass('has-no-events')){
 			var date = $(this).data('date');
 			var display = $(this).data('display');
-			$(this).append('<div class="date-popup-wrapper"><div class="date-popup"><p>'+date+'</p><h2>'+display+'</h2></div></div>');
+			$(this).append('<div class="date-popup-wrapper"><div class="date-popup"><p>'+date+'</p><h2>'+display+'</h2></div><div id="popup-ind"></div></div>');
+
+			// positioning logic
 			var pos = $(this).position();
 			var popupH = $('.date-popup-wrapper').outerHeight();
-			var popupW = $('.date-popup-wrapper').outerWidth();
-			var cellH = $(this).height();
-			var cellW = $(this).width();
-			$('.date-popup-wrapper').css({'left':pos.left-(popupW*0.5)+(cellW*0.5)+5,'top':pos.top-popupH-11}); //yeah. this is hacky
+
+			$('.date-popup-wrapper').css({'left':0,'top':pos.top-popupH-11}); //yeah. this is hacky
+			$('#popup-ind').css({'left':pos.left+8});
 		}
 	})
 
