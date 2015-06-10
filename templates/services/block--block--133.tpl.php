8e925cf790d2b811e53b7cef3f028eebf709e5d6
Erik Olson <eolson@ncsu.edu>
2015-06-10 12:48:02 -0400

    hours for hill makerspace

--- templates/services/block--block--133.tpl.php
+++ templates/services/block--block--133.tpl.php
@@ -1,6 +1,7 @@
 <section class="block block-block contextual-links-region">
     <?php
-        $mkr_ary = hours_get_day_range('hunt','makerspace',5);
+        $lib = (request_uri() == '/do/make-at-hunt') ? 'hunt' : 'hill';
+        $mkr_ary = hours_get_day_range($lib,'makerspace',5);
         // kpr($mkr_ary);
         echo '<h2>Hours</h2>';
         echo '<table>';
@@ -12,5 +13,5 @@
         }
         echo '</table>';
     ?>
-    <p><a href="/hours/hunt/makerspace">Full hours and exceptions »</a></p>
+    <p><a href="/hours/<?= $lib ?>/makerspace">Full hours and exceptions »</a></p>
 </section>
