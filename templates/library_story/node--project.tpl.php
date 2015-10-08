b0361b083f271df40d4ab8de917a44fd88c9421e
Erik Olson <eolson@ncsu.edu>
2015-10-08 17:09:50 -0400

    new story leads field

--- templates/library_story/node--project.tpl.php
+++ templates/library_story/node--project.tpl.php
@@ -46,9 +46,7 @@
             <p class="date-posted">Written on <?php print date('F j, Y', $created); ?></p>
           </div>
           <aside class="story-statement-aside sidebar">
-            <h2>Story Lead</h2>
-
-            <?php print $name; ?>
+            <?php print render($content['field_story_lead']); ?>
             <?php print render($content['field_staff']); ?>
             <?php print render($content); ?>
             <?php
