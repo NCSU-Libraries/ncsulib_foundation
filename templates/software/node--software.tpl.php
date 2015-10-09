0318c3e78f9930a5db508876791b41e2fd434bce
Erik Olson <eolson@ncsu.edu>
2015-10-08 21:54:25 -0400

    new software node

--- templates/software/node--software.tpl.php
+++ templates/software/node--software.tpl.php
@@ -1,39 +1,40 @@
 <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
-    <?php if ($title): ?>
-        <?php print render($title_prefix); ?>
-        <h1 id="page-title" class="title"><?php print $title; ?></h1>
-        <?php print render($title_suffix); ?>
-    <?php endif; ?>
+  <?php if ($title): ?>
+      <?php print render($title_prefix); ?>
+          <h1 id="page-title" class="title"><?php print $title; ?></h1>
+      <?php print render($title_suffix); ?>
+  <?php endif; ?>
 
-    <?php print render($title_prefix); ?>
-    <?php if (!$page): ?>
-        <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-    <?php endif; ?>
-    <?php print render($title_suffix); ?>
+  <?php print render($title_prefix); ?>
+  <?php if (!$page): ?>
+      <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
 
 
+  <div class="content"<?php print $content_attributes; ?>>
     <?php
-        // We hide the comments and links because we will not use them.
-        hide($content['comments']);
-        hide($content['links']);
-        hide($content['field_tags']);
+      // We hide the comments and links because we will not use them.
+      hide($content['comments']);
+      hide($content['links']);
+      hide($content['field_featured_image']);
+      hide($content['field_application_icon']);
+      hide($content['field_platform']);
+      hide($content['field_documentation_url']);
 
-        print render($content);
     ?>
 
     <div class="row">
-        <div class="columns medium-9">
-            <?= drupal_render($content['body']); ?>
-            <?= drupal_render($content['field_how_to_access']); ?>
-            <?= drupal_render($content['field_software_devices']); ?>
-            <?= drupal_render($content['field_software_spaces']); ?>
-            <?= drupal_render($content['field_documentation_urlt']); ?>
+        <div id="main-content" class="medium-9 push-3 main columns">
+            <?= render($content); ?>
         </div>
-        <div class="columns medium-3">
-            <?= drupal_render($content['field_application_icon']); ?>
-            <?= drupal_render($content['field_version']); ?>
-            <?= drupal_render($content['field_platform']); ?>
-        </div>
+        <aside id="subnav" class="medium-3 pull-9 l-sidebar-first columns sidebar" role="complementary">
+            <?= render($content['field_application_icon']); ?>
+            <?= render($content['field_platform']); ?>
+            <?= render($content['field_documentation_url']); ?>
+        </aside>
     </div>
+
+  </div>
 
 </article>
