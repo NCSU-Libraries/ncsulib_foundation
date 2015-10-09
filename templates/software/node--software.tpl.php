0aecba4c724ee8b47b903c10c2d7f226c6af9cb6
Erik Olson <eolson@ncsu.edu>
2015-10-09 09:39:48 -0400

    fix column widths

--- templates/software/node--software.tpl.php
+++ templates/software/node--software.tpl.php
@@ -1,18 +1,11 @@
 <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
-  <?php if ($title): ?>
-      <?php print render($title_prefix); ?>
-          <h1 id="page-title" class="title"><?php print $title; ?></h1>
-      <?php print render($title_suffix); ?>
-  <?php endif; ?>
+    <?php if ($title): ?>
+        <?php print render($title_prefix); ?>
+            <h1 id="page-title" class="title"><?php print $title; ?></h1>
+        <?php print render($title_suffix); ?>
+    <?php endif; ?>
 
-  <?php print render($title_prefix); ?>
-  <?php if (!$page): ?>
-      <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-  <?php endif; ?>
-  <?php print render($title_suffix); ?>
-
-
-  <div class="content"<?php print $content_attributes; ?>>
+    <div class="content"<?= $content_attributes; ?>>
     <?php
       // We hide the comments and links because we will not use them.
       hide($content['comments']);
@@ -22,19 +15,31 @@
       hide($content['field_platform']);
       hide($content['field_documentation_url']);
 
+      // if sidebar has content
+      if($content['field_application_icon'] || $content['field_platform'] || $content['field_documentation_url']){
+        $sidebar = true;
+        $main_classes = 'medium-9 push-3';
+      } else{
+        $sidebar = false;
+        $main_classes = 'medium-12';
+      }
+
     ?>
 
     <div class="row">
-        <div id="main-content" class="medium-9 push-3 main columns">
+        <div id="main-content" class="<?=$main_classes?> main columns">
             <?= render($content); ?>
         </div>
+        <?php if($sidebar){ ?>
         <aside id="subnav" class="medium-3 pull-9 l-sidebar-first columns sidebar" role="complementary">
             <?= render($content['field_application_icon']); ?>
             <?= render($content['field_platform']); ?>
             <?= render($content['field_documentation_url']); ?>
         </aside>
+        <?php } ?>
     </div>
 
-  </div>
+
+    </div>
 
 </article>
