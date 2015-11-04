aae4ae2318b42e57587ebc7c9909350d3dc83f8b
Erik Olson <eolson@ncsu.edu>
2015-11-04 11:02:48 -0500

    add hash to select

--- scripts/borrow-privileges.js
+++ scripts/borrow-privileges.js
@@ -1,8 +1,22 @@
 jQuery(function($){
-	$(".patronTypeTable").hide();
-	$("#undergradstaff").show();
+	// $(".patronTypeTable").hide();
+	// $("#undergradstaff").show();
 	$("#patronselection").change(function(){
 		$(".patronTypeTable").hide();
 		$("#"+$(this).val()).show();
-	});
+        window.location.hash = $(this).val();
+    });
+
+    hash = window.location.hash;
+    if(hash){
+        $(".patronTypeTable").hide();
+        $(hash).show();
+
+        // change select
+        h = hash.split('#');
+        $('#patronselection').val(h[1]);
+    } else{
+        console.log('no hash');
+    }
+
 });
