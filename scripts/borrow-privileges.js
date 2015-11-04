2a2bc267025ea16ff1717efad31b2c5210df3c22
Erik Olson <eolson@ncsu.edu>
2015-11-04 11:44:55 -0500

    no change

--- scripts/borrow-privileges.js
+++ scripts/borrow-privileges.js
@@ -1,6 +1,5 @@
 jQuery(function($){
-	// $(".patronTypeTable").hide();
-	// $("#undergradstaff").show();
+
 	$("#patronselection").change(function(){
 		$(".patronTypeTable").hide();
 		$("#"+$(this).val()).show();
