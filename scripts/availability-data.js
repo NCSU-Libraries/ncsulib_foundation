3c5fa66eed0132b2a78a0d4d39c1f8f00c282f72
Erik Olson <eolson@ncsu.edu>
2015-12-18 10:09:50 -0500

    pull device data from sirsi_data

--- scripts/availability-data.js
+++ scripts/availability-data.js
@@ -7,76 +7,83 @@ jQuery(document).ready(function($){
 // Load Availability Data
 var tech = {
 
-  init : function(){
-    tech.loadTechJSON();
-    tech.loadSpaceJson();
-  },
+    laptopAry : ['2081286', '2729663'],
+    tabletAry : ['2260031','3347659','2736849'],
 
-  loadTechJSON : function(){
-    // Fetching tech lending data
-    jQuery.getJSON('/sites/all/modules/custom/sirsi_data/master.json', function(data) {
-      // laptops
-      macs = data['2081286']
-      huntmacs = macs['HUNT']['LAPTOP-PAT']['TECHLEND'];
-      hillmacs = macs['DHHILL']['LAPTOP-PAT']['TECHLEND'];
-      pcs = data['2729663'];
-      huntpcs = pcs['HUNT']['LAPTOP-PAT']['TECHLEND'];
-      hillpcs = pcs['DHHILL']['LAPTOP-PAT']['TECHLEND'];
-      chromebooks = data['3310348'];
-      huntCbook = 0;hillCbook = 0;
-      if(chromebooks['HUNT']['EQUIP-1WK']['TECHLEND']){huntCbook += chromebooks['HUNT']['EQUIP-1WK']['TECHLEND'];}
-      if(chromebooks['HUNT']['EQUIP-4HR']['TECHLEND']){huntCbook += chromebooks['HUNT']['EQUIP-4HR']['TECHLEND'];}
-      if(chromebooks['DHHILL']['EQUIP-1WK']['TECHLEND']){hillCbook += chromebooks['DHHILL']['EQUIP-1WK']['TECHLEND'];}
-      if(chromebooks['DHHILL']['EQUIP-4HR']['TECHLEND']){hillCbook += chromebooks['DHHILL']['EQUIP-4HR']['TECHLEND'];}
+    init : function(){
+        tech.loadTechJSON();
+        // tech.loadSpaceJson();
+    },
 
+    loadTechJSON : function(){
+        // Fetching tech lending data
+        jQuery.getJSON('//'+tech.getDomain()+'.lib.ncsu.edu/sites/all/modules/custom/sirsi_data/master.json', function(data) {
+            tech.data = data;
+            tech.loadLaptops();
+            tech.loadTablets();
+        }, 'json');
+    },
 
-      // tablets
-      ipads = data['2260031'];
-      huntIpad=0;hillIpad=0;
-      if(ipads['HUNT']['EQUIP-1WK']['TECHLEND']){huntIpad += ipads['HUNT']['EQUIP-1WK']['TECHLEND'];}
-      if(ipads['HUNT']['EQUIP-4HR']['TECHLEND']){huntIpad += ipads['HUNT']['EQUIP-4HR']['TECHLEND'];}
-      if(ipads['DHHILL']['EQUIP-1WK']['TECHLEND']){hillIpad += ipads['DHHILL']['EQUIP-1WK']['TECHLEND'];}
-      if(ipads['DHHILL']['EQUIP-4HR']['TECHLEND']){hillIpad += ipads['DHHILL']['EQUIP-4HR']['TECHLEND'];}
-      nexus = data['3347659'];
-      huntNexus=0;hillNexus=0;
-      if(nexus['HUNT']['EQUIP-1WK']['TECHLEND']){huntNexus += nexus['HUNT']['EQUIP-1WK']['TECHLEND'];}
-      if(nexus['DHHILL']['EQUIP-1WK']['TECHLEND']){hillNexus += nexus['DHHILL']['EQUIP-1WK']['TECHLEND'];}
-      surface = data['2736849'];
-      huntSurface=0;hillSurface=0;
-      if(surface['HUNT']['EQUIP-1WK']['TECHLEND']){huntSurface += surface['HUNT']['EQUIP-1WK']['TECHLEND'];}
-      if(surface['HUNT']['EQUIP-4HR']['TECHLEND']){huntSurface += surface['HUNT']['EQUIP-4HR']['TECHLEND'];}
-      if(surface['DHHILL']['EQUIP-1WK']['TECHLEND']){hillSurface += surface['DHHILL']['EQUIP-1WK']['TECHLEND'];}
-      // if(surface['DHHILL']['EQUIP-4HR']['TECHLEND']){hillSurface += surface['DHHILL']['EQUIP-4HR']['TECHLEND'];}
+    loadLaptops : function(){
+        hill = 0; hunt = 0;
+        for(i=0;i<tech.laptopAry.length;i++){
+            hill += tech.data[tech.laptopAry[i]]['DHHILL']['LAPTOP-PAT']['TOTAL'];
+            hunt += tech.data[tech.laptopAry[i]]['HUNT']['LAPTOP-PAT']['TOTAL'];
+        }
+        jQuery('#hill-laptops').text(hill);
+        jQuery('#hunt-laptops').text(hunt);
+    },
 
-      jQuery('#hill-laptops').html(hillmacs+hillpcs+hillCbook);
-      jQuery('#hunt-laptops').html(huntmacs+huntpcs+huntCbook);
+    loadTablets : function(){
+        hill = 0; hunt = 0;
+        for(i=0;i<tech.tabletAry.length;i++){
+            // console.log(tech.data[tech.tabletAry[i]]);
+            hill += tech.data[tech.tabletAry[i]]['DHHILL']['EQUIP-1WK']['TOTAL'];
 
-      jQuery('#hill-tablets').html(hillIpad+hillNexus+hillSurface);
-      jQuery('#hunt-tablets').html(huntIpad+huntNexus+huntSurface);
+            if(typeof tech.data[tech.tabletAry[i]]['DHHILL']['EQUIP-4HR'] !== 'undefined'){
+                hill += tech.data[tech.tabletAry[i]]['DHHILL']['EQUIP-4HR']['TOTAL'];
+            }
 
-    }, 'json');
-  },
+            hunt += tech.data[tech.tabletAry[i]]['HUNT']['EQUIP-1WK']['TOTAL'];
 
-  loadSpaceJson : function(){
-    // Fetching hill spaces data
-    jQuery.getJSON('/website/sra/hill.json', function(data) {
-      tech.writeHillSpaces(data);
-    }, 'json');
+            if(typeof tech.data[tech.tabletAry[i]]['HUNT']['EQUIP-4HR'] !== 'undefined'){
+                hunt += tech.data[tech.tabletAry[i]]['HUNT']['EQUIP-4HR']['TOTAL'];
+            }
+        }
+        jQuery('#hill-tablets').text(hill);
+        jQuery('#hunt-tablets').text(hunt);
+    },
 
-    // Fetching hunt spaces data
-    jQuery.getJSON('/website/sra/hunt.json', function(data) {
-      tech.writeHuntSpaces(data);
-    }, 'json');
-  },
+    loadSpaceJson : function(){
+        // Fetching hill spaces data
+        jQuery.getJSON('/website/sra/hill.json', function(data) {
+            tech.writeHillSpaces(data);
+        }, 'json');
 
-  writeHillSpaces : function(spaceData){
-    var available = spaceData['hill']['available'];
-    jQuery('#hill-study').html(available);
-  },
+        // Fetching hunt spaces data
+        jQuery.getJSON('/website/sra/hunt.json', function(data) {
+            tech.writeHuntSpaces(data);
+            }, 'json');
+        },
 
-  writeHuntSpaces : function(spaceData){
-    var available = spaceData['hunt']['available'];
-    jQuery('#hunt-study').html(available);
-  }
+    writeHillSpaces : function(spaceData){
+        var available = spaceData['hill']['available'];
+        jQuery('#hill-study').html(available);
+    },
+
+    writeHuntSpaces : function(spaceData){
+        var available = spaceData['hunt']['available'];
+        jQuery('#hunt-study').html(available);
+    },
+
+    getDomain : function(){
+        var full = window.location.host;
+
+        var parts = full.split('.');
+        var sub = parts[0];
+        var domain = parts[1];
+
+        return sub;
+    }
 
 };
