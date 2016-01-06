6ffdd8385212e9e542315ef33257559f758cdd2b
Erik Olson <eolson@ncsu.edu>
2016-01-06 17:01:28 -0500

    test for href

--- scripts/event-series-hack.js
+++ scripts/event-series-hack.js
@@ -2,7 +2,7 @@ var events = {
     init : function(){
         var oldId;
         jQuery('.view-upcoming-events .view-content .views-row').each(function(i){
-            id = jQuery(this).find('.event-photo a').attr('href');
+            id = jQuery(this).find('.event-info h3 a').attr('href');
             console.log(id)
             if(id === oldId){
                 jQuery(this).hide();
