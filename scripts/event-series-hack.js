8303647c2b47c65fb38e61b98bae40dafa7ed8fc
Erik Olson <eolson@ncsu.edu>
2016-02-03 15:22:49 -0500

    remove dupes in prev events

--- scripts/event-series-hack.js
+++ scripts/event-series-hack.js
@@ -1,7 +1,7 @@
 var events = {
     init : function(){
         var oldId;
-        jQuery('.view-upcoming-events .view-content .views-row').each(function(i){
+        jQuery('.view-upcoming-events .view-content .views-row,.view-upcoming-events .view-content .event-item').each(function(i){
             id = jQuery(this).find('.event-info h3 a').attr('href');
             if(id === oldId){
                 jQuery(this).hide();
