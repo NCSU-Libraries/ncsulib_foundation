aa17902502e203255eff5ab8d87d56a0a8fedd67
Erik Olson <eolson@ncsu.edu>
2016-02-03 13:50:50 -0500

    space changes. add new usage policies

--- templates/roomfinder/node--space.tpl.php
+++ templates/roomfinder/node--space.tpl.php
@@ -11,7 +11,7 @@
   <?php endif; ?>
   <?php print render($title_suffix); ?>
 
-  <div class="content row"<?php print $content_attributes; ?>>
+  <div class="content "<?php print $content_attributes; ?>>
     <?php
         // We hide the comments and links because we will not use them.
         hide($content['comments']);
@@ -22,25 +22,63 @@
         hide($content['field_building_new_']);
         hide($content['field_room_number']);
         hide($content['field_capacity']);
-        hide($content['field_contact_entity_phone']);
         hide($content['field_floor']);
+        hide($content['field_contact_entity_name']);
+        hide($content['field_contact_entity_phone']);
+        hide($content['field_contact_entity_email']);
+        hide($content['field_contact_person']);
     ?>
 
-    <div class="columns medium-8">
-        <div class="row">
-            <div class="columns medium-12">
-                <?= render($content['field_image']); ?>
-            </div>
+
+    <!-- <div class="columns medium-12"> -->
+
+
+    <div class="row">
+        <div class="columns medium-8">
+            <?= render($content['field_image']); ?>
         </div>
-        <?= render($content); ?>
-    </div>
-    <div class="columns medium-4">
-        <?= render($content['field_building_new_']); ?>
-        <?= render($content['field_capacity']); ?>
-        <?= render($content['field_floor']); ?>
-        <?= render($content['field_room_number']); ?>
-        <?= render($content['field_contact_entity_phone']); ?>
-        <?= render($content['field_reservation_method']); ?>
+        <div class="columns medium-4">
+            <?= render($content['field_building_new_']); ?>
+            <?= render($content['field_capacity']); ?>
+            <?= render($content['field_floor']); ?>
+            <?= render($content['field_room_number']); ?>
+            <?= render($content['field_reservation_method']); ?>
+        </div>
+
+        <div class="columns medium-12">
+            <?= render($content); ?>
+
+            <h3>Usage Policies</h3>
+            <?php if($content['field_days_in_advance']): ?>
+            <p>Reservable up to <?= render($content['field_days_in_advance']); ?> in advance.</br>
+            <?php endif; ?>
+
+            <?php if($content['field_max_reservation_period']): ?>
+            Reserve for up to <?= render($content['field_max_reservation_period']); ?></br>
+            <?php endif; ?>
+
+            <?php
+
+                if($content['field_reservable_by']){
+                    $string = strip_tags(implode(', ',explode('</div>',render($content['field_reservable_by']))));
+                    echo 'Reservable by '.substr($string, 0, -4).'</br>';
+                }
+
+                if(render($content['field_15_minute_grace_period']) == 'yes'){
+                    echo 'After 15 minute grace period for arrival, space becomes available to others.';
+                }
+            ?>
+            </p>
+
+
+            <h3>Contact Info</h3>
+            <?= render($content['field_contact_point']); ?>
+            <?= render($content['field_contact_person']); ?>
+            <p><?= render($content['field_contact_entity_name']); ?></p>
+            <?= render($content['field_contact_entity_phone']); ?>
+            <?= render($content['field_contact_entity_email']); ?>
+        </div>
+
     </div>
 
   </div>
