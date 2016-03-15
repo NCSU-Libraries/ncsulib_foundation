c9ff548e6909e06394c68dc4b609ca06d378dda9
Matthew O'Connell <mtoconne@ncsu.edu>
2016-03-14 22:55:04 -0400

    Working copy of Amazon Nav JS file has been made

--- scripts/amazon-nav.js
+++ scripts/amazon-nav.js
@@ -1,90 +1,75 @@
-/*var amazonNav = {
+var amazonNav = {
+    // amount of time (in ms) for mouse to hover before opening subnav
+    navEnterTimeout : 55,
+
+    // amount of time (in ms) for mouse to hover before opening a new subnav
+    mouseoverTimout : 70,
+    
+    pauseBeforeOpening : false,
+
 	init : function() {
+	
+        // mouse entered main nav
+        $('ul.primary-nav>li').mouseenter(function(e){
+            var navIndex = $(this).index();
 
-        console.log('test');
+            if ( amazonNav.pauseBeforeOpening ) {
+                // the nav is already open, but mouse went over new main nav item
+                // don't open immediately in case they are mousing over a main nav corner
+                amazonNav.pauseBeforeOpening = false;
+                setTimeout(function(){
+                    if ( $(e.target).is(":hover") ) {
+                        amazonNav.openNavItem(navIndex);
+                    }
+                }, amazonNav.navEnterTimeout);
+            } else {
+                // the subnav is not open yet
+                // don't open immediately in case they are mousing over the main nav
+                setTimeout(function(){
+                    if ( $(e.target).is(":hover") ) {
+                        amazonNav.openNavItem(navIndex);
+                    }
+                }, amazonNav.mouseoverTimout);
+            }
+        });
+        
+        // mouse left the main nav
+        $('ul.primary-nav>li').mouseleave(function(e){
+            if( $(e.toElement || e.relatedTarget).hasClass('primary-menu-item') ) {
+                // mouse left the nav and went into another one
+                // set variable so that it doesn't open immediately
+                amazonNav.pauseBeforeOpening = true;
+            } else if ( ! $(e.toElement || e.relatedTarget).hasClass('primary-menu-list') ) {
+                // mouse left the menu completely
+                amazonNav.closeNav();
+            }
+        });
+        
+        // mouse left the subnav
+        $('.primary-menu-list').mouseleave(function(e){
+            if( ! $(e.toElement || e.relatedTarget).hasClass('primary-menu-item') ) {
+                // mouse left the nav completely
+                amazonNav.closeNav();
+            }
+        });
 
-	}
+	},
+
+
+    closeNav : function() {
+        $('.primary-menu-item').removeClass('open');
+        $('.primary-menu-list').removeClass('open');
+    },
+
+    openNavItem : function(navIndex) {
+        // closing all nav will avoid nav items stacking on each other
+        amazonNav.closeNav();
+        $('.primary-menu-item:eq(' + navIndex + ')').addClass('open');
+        $('.primary-menu-list:eq(' + navIndex + ')').addClass('open');
+    }	
+	
 };
 
 jQuery(function(){
     amazonNav.init();
-});*/
-
-
-
-$(document).ready(function(){
-    
-        var timer;
-        var stopProp = false;
-        $('ul.primary-nav>li').mouseenter(function(e){
-            var navIndex = $(this).index();
-            // NAV NOT OPEN
-            if ( stopProp ) {
-                //alert('stop and make sure it stays there for a second');
-                stopProp = false;
-                setTimeout(function(){
-                    if ( $(e.target).is(":hover") ) {
-                        openNavItem(navIndex);
-                    }
-                }, 55);
-            } else {
-                if ( ! $('#primary-nav-menus').hasClass('open') ) {
-                    setTimeout(function(){
-                        if ( $(e.target).is(":hover") ) {
-                            openNavItem(navIndex);
-                        }
-                    },70);
-                    //alert('its not open');
-                } else if ( onPrimaryNav ) {
-                    // we are leaving one nav for another (set timeout)
-                    //alert('one nav for another');
-                } else {
-                    //$(e.target).addClass('open');
-                    openNavItem(navIndex);
-                }
-            }
-        });
-        
-        
-        // this is coming from the nav links at the top
-        $('ul.primary-nav>li').mouseleave(function(e){
-            if( $(e.toElement).hasClass('primary-menu-item') ) {
-                // left the nav and went into another one
-                //alert('it went into a primary-menu-item');
-                stopProp = true;
-            } else if ( $(e.toElement).hasClass('primary-menu-list') ) {
-                // left the nav and went into the menu list
-                //alert('it went into a primary-menu-list');
-            } else {
-                // left the menu links completely
-                closeNav();
-            }
-        });
-        
-        
-        // left the menu list
-        $('.primary-menu-list').mouseleave(function(e){
-            if( ! $(e.toElement).hasClass('primary-menu-item') ) {
-                closeNav();
-            }
-        });
-        
-        
-    });
-
-    function closeOneNav() {
-        $('.primary-menu-item:eq(' + navIndex + ')').removeClass('open');
-        $('.primary-menu-list:eq(' + navIndex + ')').removeClass('open');
-    }
-    
-    function closeNav() {
-        $('.primary-menu-item').removeClass('open');
-        $('.primary-menu-list').removeClass('open');
-    }
-
-    function openNavItem(navIndex) {
-        //alert('open nav item ' + navIndex);
-        closeNav();
-        $('.primary-menu-item:eq(' + navIndex + ')').addClass('open');
-        $('.primary-menu-list:eq(' + navIndex + ')').addClass('open');
-    }
+});
