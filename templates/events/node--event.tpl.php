1ad6adef8041c1d4971065162ccfb655a539329a
Erik Olson <eolson@ncsu.edu>
2016-05-26 13:57:48 -0400

    simplify event dates in views

--- templates/events/node--event.tpl.php
+++ templates/events/node--event.tpl.php
@@ -6,19 +6,11 @@
   <h1 id="page-title" class="title" itemprop="name"><?php print $title; ?></h1>
   <?= render($title_suffix); ?>
 <?php endif; ?>
-<div>
+<div id="event-share">
     <a id="fb-share-button" data-url="<?= "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]" ?>" href="#"><i class="fa fa-facebook-square fa-2x"></i></a>
     <a id="tw-share-button" data-url="<?= "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]" ?>" href="#"><i class="fa fa-twitter-square fa-2x"></i></a>
 </div>
 <?php
-    $event_date = strtotime($content['field_time']['#object']->field_time['und'][0]['value2']);
-    $is_ongoing = ($content['field_ongoing']['#object']->field_ongoing['und'][0]['value'] == 1) ? true : false;
-
-    if($event_date && $event_date < date('U') && $is_ongoing != '1'){
-        print '<div data-alert class="alert-box">';
-        print '<i class="fa fa-warning"></i> This event has already happened.';
-        print '</div>';
-    }
 
     //is there a sidebar?
     if($node->field_image_for_event || $content['field_event_leads']){
@@ -40,35 +32,19 @@
         <div class="columns <?=$first_column?>">
             <div class="event-meta">
                 <?php
-                    // get time
-                    $t = field_get_items('node', $node, 'field_time');
-                    $adjust = ncsulib_foundation_adjust_for_timezone($t[0]['value']);
-                    $start_raw = strtotime($t[0]['value'])+$adjust;
-                    $end_raw = strtotime($t[0]['value2'])+$adjust;
-                    $duration = $end_raw - $start_raw;
-                    $d_hour = floor($duration/60/60);
-                    $d_min = ($duration/60/60 - $d_hour)*60;
+                        //did this event already happen?
+                        $event_date = strtotime($content['field_time']['#object']->field_time['und'][0]['value2']);
+                        $is_ongoing = ($content['field_ongoing']['#object']->field_ongoing['und'][0]['value'] == 1) ? true : false;
 
-                    // get categories to separate out exhibits
-                    $cat = field_get_items('node', $node, 'field_event_category');
-                    foreach($cat as $c){
-                        $time = ($c['value'] == 'Exhibits' && date('z',$start_raw) != date('z',$end_raw)) ? '' : date('g:ia',$start_raw).' - '.date('g:ia',$end_raw);
-                    }
-                    if(!$cat){$time = date('g:ia',$start_raw).' - '.date('g:ia',$end_raw);}
-                    $str = (date('z',$start_raw) == date('z',$end_raw)) ? date('F j, Y',$start_raw) : date('F j',$start_raw).' - '.date('F j, Y',$end_raw);
-                    if(!$is_ongoing){
-                        echo '<h3>When</h3>';
-                        echo '<p>';
-                        echo '<time itemprop="startDate" datetime="'.date('Y-m-d',$start_raw).'T'.date('G:i',$start_raw).'">'.$str.'<br/>'.$time.'</time>';
-                        echo '<time itemprop="duration" datetime="T'.$d_hour.'H'.$d_min.'M"></time>';
-                        echo '<time itemprop="duration" datetime="T'.$d_hour.'H'.$d_min.'M"></time>';
-                        echo '</p>';
-                    }
+                        if($event_date && $event_date < date('U') && $is_ongoing != '1'){
+                            print '<div data-alert class="alert-box">';
+                            print '<i class="fa fa-warning"></i> This event has already happened.';
+                            print '</div>';
+                        }
                 ?>
-
+                <?= drupal_render($content['field_time']); ?>
                 <?= drupal_render($content['field_space']); ?>
                 <?= drupal_render($content['field_non_libraries_space']); ?>
-
             </div>
 
             <div itemprop="description"><?php print drupal_render($content['body']); ?></div>
@@ -93,6 +69,8 @@
         <div class="columns medium-5">
             <?php if($node->field_image_for_event): ?>
             <?= '<img property="og:image" src="'.image_style_url('large', $node->field_image_for_event['und'][0]['uri']).'" width="100%" itemprop="image" />' ?>
+            <small>caption text </small>
+            <?= $content['field_image_for_event'][0]['item']['alt']; ?>
             <?php endif; ?>
 
             <?= drupal_render($content['field_event_leads']); ?>
