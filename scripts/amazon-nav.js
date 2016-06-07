980c203bdf1296768ae7de8f362092aa432d9de4
Matthew O'Connell <mtoconne@ncsu.edu>
2016-06-06 21:18:03 -0400

    Fixed touchscreen bug where subnav was opening on mobile

--- scripts/amazon-nav.js
+++ scripts/amazon-nav.js
@@ -10,77 +10,81 @@ var amazonNav = {
 
 	init : function() {
 
-	    // console.log(amazonNav.navEnterTimeout);
-
-        // mouse entered main nav
-        $('ul.primary-nav>li').bind('mouseenter', function(e){
-            var navIndex = $(this).index();
-            if ( amazonNav.pauseBeforeOpening ) {
-                // the nav is already open, but mouse went over new main nav item
-                // don't open immediately in case they are mousing over a main nav corner
-                amazonNav.pauseBeforeOpening = false;
-                setTimeout(function(){
-                    if ( $(e.target).is(":hover") ) {
-                        amazonNav.openNavItem(navIndex);
+            // mouse entered main nav
+            $('ul.primary-nav>li').bind('mouseenter', function(e){
+                // if we are on the large screen
+                if ( $('#nav-toggle').css('display') == 'none' ) {
+                    var navIndex = $(this).index();
+                    if ( amazonNav.pauseBeforeOpening ) {
+                        // the nav is already open, but mouse went over new main nav item
+                        // don't open immediately in case they are mousing over a main nav corner
+                        amazonNav.pauseBeforeOpening = false;
+                        setTimeout(function(){
+                            if ( $(e.target).is(":hover") ) {
+                                amazonNav.openNavItem(navIndex);
+                            }
+                        }, amazonNav.mouseoverTimeout);
+                    } else {
+                        // the subnav is not open yet
+                        // don't open immediately in case they are mousing over the main nav
+                        setTimeout(function(){
+                            if ( $(e.target).is(":hover") ) {
+                                amazonNav.openNavItem(navIndex);
+                            }
+                        }, amazonNav.navEnterTimeout);
                     }
-                }, amazonNav.mouseoverTimeout);
-            } else {
-                // the subnav is not open yet
-                // don't open immediately in case they are mousing over the main nav
-                setTimeout(function(){
-                    if ( $(e.target).is(":hover") ) {
-                        amazonNav.openNavItem(navIndex);
-                    }
-                }, amazonNav.navEnterTimeout);
-            }
-        });
+                }
+            });
 
-        // touchdown main nav
-        $('ul.primary-nav>li').bind('touchstart', function(e){
-            e.preventDefault();
-            var navIndex = $(this).index();
-            if ( amazonNav.pauseBeforeOpening ) {
-                amazonNav.pauseBeforeOpening = false;
-            }
-            if ( $(this).find('a').hasClass('open') ) {
-                // they already have the nav open so open the main nav item
-                window.open( 'http:' + $(this).find('a').attr('href'),'_self' );
-            } else {
-                // the subnav is not open yet
-                // don't open immediately in case they are mousing over the main nav
-                setTimeout(function(){
-                    amazonNav.openNavItem(navIndex);
-                }, amazonNav.navEnterTimeout);
-            }
-        });
+            // touchdown main nav
+            $('ul.primary-nav>li').bind('touchstart', function(e){
+                if ( $('#nav-toggle').css('display') == 'none' ) {
+                    e.preventDefault();
+                    var navIndex = $(this).index();
+                    if ( amazonNav.pauseBeforeOpening ) {
+                        amazonNav.pauseBeforeOpening = false;
+                    }
+                    if ( $(this).find('a').hasClass('open') ) {
+                        // they already have the nav open so open the main nav item
+                        window.open( 'http:' + $(this).find('a').attr('href'),'_self' );
+                    } else {
+                        // the subnav is not open yet
+                        // don't open immediately in case they are mousing over the main nav
+                        setTimeout(function(){
+                            amazonNav.openNavItem(navIndex);
+                        }, amazonNav.navEnterTimeout);
+                    }
+                }
+            });
         
-        $(document).bind('touchstart', function(e) {
-            if ( $(e.target).closest('ul.primary-nav').length === 0 && $(e.target).closest('.primary-menu-list').length === 0 && $('ul.primary-nav>li>a').hasClass('open') ) {
-                e.preventDefault();
-                amazonNav.closeNav();
-            }
-        });
+            $(document).bind('touchstart', function(e) {
+                if ( $(e.target).closest('ul.primary-nav').length === 0 && $(e.target).closest('.primary-menu-list').length === 0 && $('ul.primary-nav>li>a').hasClass('open') && $('#nav-toggle').css('display') == 'none' ) {
+                    e.preventDefault();
+                    amazonNav.closeNav();
+                }
+            });
 
 
-        // mouse left the main nav
-        $('ul.primary-nav>li').bind('mouseleave', function(e){
-            if( $(e.toElement || e.relatedTarget).hasClass('primary-menu-item') ) {
-                // mouse left the nav and went into another one
-                // set variable so that it doesn't open immediately
-                amazonNav.pauseBeforeOpening = true;
-            } else if ( ! $(e.toElement || e.relatedTarget).hasClass('primary-menu-list') ) {
-                // mouse left the menu completely
-                amazonNav.closeNav();
-            }
-        });
+            // mouse left the main nav
+            $('ul.primary-nav>li').bind('mouseleave', function(e){
+                if( $(e.toElement || e.relatedTarget).hasClass('primary-menu-item') ) {
+                    // mouse left the nav and went into another one
+                    // set variable so that it doesn't open immediately
+                    amazonNav.pauseBeforeOpening = true;
+                } else if ( ! $(e.toElement || e.relatedTarget).hasClass('primary-menu-list') ) {
+                    // mouse left the menu completely
+                    amazonNav.closeNav();
+                }
+            });
 
-        // mouse left the subnav
-        $('.primary-menu-list').bind('mouseleave', function(e){
-            if( ! $(e.toElement || e.relatedTarget).hasClass('primary-menu-item') ) {
-                // mouse left the nav completely
-                amazonNav.closeNav();
-            }
-        });
+            // mouse left the subnav
+            $('.primary-menu-list').bind('mouseleave', function(e){
+                if( ! $(e.toElement || e.relatedTarget).hasClass('primary-menu-item') ) {
+                    // mouse left the nav completely
+                    amazonNav.closeNav();
+                }
+            });
+        
 
 	},
 
