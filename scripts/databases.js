1c4941d08e6e9af4be4e1fe65b0814e857c7dad3
Erik Olson <eolson@ncsu.edu>
2016-07-07 13:03:55 -0400

    various styling updates

--- scripts/databases.js
+++ scripts/databases.js
@@ -2,24 +2,76 @@ var db = {
     init : function(){
         db.handleSideNav();
         db.handleDbSort();
+        db.handleSearchInput();
+    },
+
+    handleSearchInput : function(){
+        //run search when a key is pressed in the search box
+        $('#db-search-box input').on('keyup',function(e){
+            val = $(this).val().toLowerCase();
+            var num = 0;
+            $('.db-list ul li').each(function(i){
+                title = $(this).find('h3 a').text();
+                if(title.toLowerCase().indexOf(val) == -1){
+                    //hide is not found in string
+                    $(this).find('.db').hide();
+                } else if(val == '' || val == ' '){ //empty search box
+                    $(this).find('.db').hide();
+                } else{
+                    //show total results with search string
+                    num++;
+                    //show all db's
+                    $(this).find('.db').show();
+                }
+            })
+
+            //results msg
+            if(val == '' || val == ' '){
+                $('#db-result').text("Please try a search or choose a category");
+            } else if(num == 0){
+                $('#db-result').text("We got nothin'. Please retry your search or choose a category");
+            } else{
+                $('#db-result').text('Showing '+num+' databases for "'+val+'"');
+            }
+
+        });
     },
 
     handleSideNav : function(){
         $('.item-list h3').on('click', function(e){
 
-            // //close all navs
-            db.closeAllNavItems();
-            // $('.subject-list').hide();
+            var tid = $(this).find('.subject-title').data('tid');
+            var target = $(e.target);
 
-            if($(this).hasClass('active')){
-                // close item nav
-                $(this).removeClass('active');
-                $(this).next().slideUp();
-            } else{
-                // open item nav
-                $('.item-list h3').removeClass('active');
-                $(this).addClass('active');
-                $(this).next().slideDown();
+
+
+            if($(target).hasClass('subject-title')){ //if clicking on term name
+                // hide all items
+                $('.db-list li .db').hide();
+                $('.db-list li div.'+tid).show();
+                $('.subject-list a').removeClass('selected');
+
+                if($(this).hasClass('active')){
+                    // close item nav
+                    $(this).removeClass('active');
+                } else{
+                    // open item nav
+                    $('.item-list h3').removeClass('active');
+                    $(this).addClass('active');
+                    $('#db-result').text('Showing '+db.getNumVisibleDBs()+' databases for "'+$(this).text().trim()+'"');
+                }
+            } else if($(target).hasClass('nav-expand')){ //if clicking on expand button
+                //close all navs
+                $('.subject-list').slideUp();
+
+                if($(target).hasClass('active')){
+                    // close item nav
+                    $(target).removeClass('active');
+                } else{
+                    // open item nav
+                    $(target).addClass('active');
+                    $(target).parent().next().slideDown();
+                }
             }
 
             e.preventDefault();
@@ -27,6 +79,8 @@ var db = {
     },
 
     handleDbSort : function(){
+        $('#db-result').text('Showing '+db.getNumVisibleDBs()+' core databases. Enter a search or choose a category for more.');
+
         $('.subject-list a').on('click', function(e){
 
             tid = $(this).data('tid');
@@ -34,19 +88,20 @@ var db = {
             // hide all items
             $('.db-list li .db').hide();
             $('.subject-list a').removeClass('selected');
+            $('.item-list h3').removeClass('active');
 
             // only show items clicked on
             $(this).addClass('selected');
             $('.db-list li div.'+tid).show();
 
+            $('#db-result').text('Showing '+db.getNumVisibleDBs()+' databases for "'+$(this).text().trim()+'"');
+
             e.preventDefault();
         })
     },
 
-    closeAllNavItems : function(){
-        $('.item-list').each(function(i){
-            $(this).find('ul').slideUp();
-        })
+    getNumVisibleDBs : function(){
+        return $('.db-list li div:visible').length;//number of visible db's
     }
 }
 
