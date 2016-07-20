660ccdfeda890caf51bdf5d6a5ba67b346d8bb41
Erik Olson <eolson@ncsu.edu>
2016-07-20 10:42:24 -0400

    iframe auto height

--- scripts/namingopps.js
+++ scripts/namingopps.js
@@ -201,7 +201,4 @@ jQuery(function($) {
 function resizeIframe(obj){
    obj.style.height = 0;
    obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
-  // setInterval(function(){
-  console.log(obj.contentWindow.document.body.scrollHeight);
-  // }, 1000)
 }
