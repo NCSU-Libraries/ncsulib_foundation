b171b229bbed1a0814a07cd3d9fd63a973de6bd8
Erik Olson <eolson@ncsu.edu>
2016-08-08 11:00:56 -0400

    add animation to accordion

--- scripts/music-rooms.js
+++ scripts/music-rooms.js
@@ -7,9 +7,9 @@ var music = {
             e.preventDefault();
         })
 
-        $(".accordion").on("click", "li:first-child", function (event) {
+        $(".accordion").on("click", "li", function (event) {
  			$("div.active").slideToggle("slow");
- 			$(this).find(".content").slideToggle("slow");
+ 			$(this).find(".content").first().slideToggle("slow");
  		});
     }
 }
